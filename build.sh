37c9864ecd822dc726bcfe3969ad633affa86d2f
Craig Peterson <cpeterson@stackoverflow.com>
2016-08-25 17:03:32 -0600

    adding some build scripts

---
+++ build.sh
@@ -0,0 +1,23 @@
+
+PKG=github.com/StackExchange/dnscontrol
+FLAGS="-s -w"
+
+echo 'Building Linux'
+go build -o dnscontrol-Linux -ldflags "$FLAGS" $PKG
+
+echo 'Building Windows'
+export GOOS=windows
+go build -o dnscontrol.exe -ldflags "$FLAGS" $PKG
+
+echo 'Building Darwin'
+export GOOS=darwin
+go build -o dnscontrol-Darwin -ldflags "$FLAGS" $PKG
+
+ls -lah dnscontrol*
+
+echo 'Compressing executables'
+upx dnscontrol.exe
+upx dnscontrol-Linux
+upx dnscontrol-Darwin
+
+ls -lah dnscontrol*
