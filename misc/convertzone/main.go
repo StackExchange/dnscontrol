c92d339a90242168d698681de1eb470e9d1b33e8
Tom Limoncelli <tlimoncelli@stackoverflow.com>
2017-03-20 14:26:28 -0400

    Linting.

--- misc/convertzone/main.go
+++ misc/convertzone/main.go
@@ -15,7 +15,6 @@ convertzone: Read BIND-style zonefile and output.
 
 import (
 	"bufio"
-	"errors"
 	"flag"
 	"fmt"
 	"io"
@@ -27,51 +26,55 @@ import (
 	"github.com/StackExchange/dnscontrol/providers/bind"
 	"github.com/miekg/dns"
 	"github.com/miekg/dns/dnsutil"
+	"github.com/pkg/errors"
 )
 
-var output_mode = flag.String("mode", "tsv", "tsv|dsl|pretty")
-var flag_defaultTtl = flag.Uint("ttl", 300, "Default TTL")
-var flag_registrar = flag.String("registrar", "REG_FILL_IN", "registrar text")
-var flag_provider = flag.String("provider", "DNS_FILL_IN", "provider text")
+var flagMode = flag.String("mode", "tsv", "tsv|dsl|pretty")
+var flagDefaultTTL = flag.Uint("ttl", 300, "Default TTL")
+var flagRegText = flag.String("registrar", "REG_FILL_IN", "registrar text")
+var flagProviderText = flag.String("provider", "DNS_FILL_IN", "provider text")
 
 // parseargs parses the non-flag arguments.
 func parseargs(args []string) (zonename string, filename string, r io.Reader, err error) {
 	// 1 args: first arg is the zonename. Read stdin.
 	// 2 args: first arg is the zonename. 2nd is the filename.
+	// Anything else returns an error.
 
 	if len(args) < 1 {
-		return "", "", nil, errors.New("Not enough parameters")
+		return "", "", nil, fmt.Errorf("no command line parameters. Zone name required")
 	}
 
 	zonename = args[0]
 
-	if len(args) < 2 {
+	if len(args) == 1 {
 		filename = "stdin"
 		r = bufio.NewReader(os.Stdin)
-	} else {
+	} else if len(args) == 2 {
 		filename = flag.Arg(1)
 		r, err = os.Open(filename)
 		if err != nil {
-			log.Fatal(err)
+			return "", "", nil, errors.Wrapf(err, "Could not open file: %s", filename)
 		}
+	} else {
+		return "", "", nil, fmt.Errorf("too many command line parameters")
 	}
 
 	return zonename, filename, r, nil
 }
 
 // pretty outputs the zonefile using the prettyprinter.
-func pretty(zonename string, filename string, r io.Reader, defaultTtl uint32) {
-	var pretty_list []dns.RR
+func pretty(zonename string, filename string, r io.Reader, defaultTTL uint32) {
+	var l []dns.RR
 	for x := range dns.ParseZone(r, zonename, filename) {
 		if x.Error == nil {
-			pretty_list = append(pretty_list, x.RR)
+			l = append(l, x.RR)
 		}
 	}
-	bind.WriteZoneFile(os.Stdout, pretty_list, zonename, defaultTtl)
+	bind.WriteZoneFile(os.Stdout, l, zonename, defaultTTL)
 }
 
-// rr_format outputs the zonefile in either DSL or TSV format.
-func rr_format(zonename string, filename string, r io.Reader, defaultTtl uint32, dsl bool) {
+// rrFormat outputs the zonefile in either DSL or TSV format.
+func rrFormat(zonename string, filename string, r io.Reader, defaultTTL uint32, dsl bool) {
 	zonenamedot := zonename + "."
 
 	for x := range dns.ParseZone(r, zonename, filename) {
@@ -108,8 +111,8 @@ func rr_format(zonename string, filename string, r io.Reader, defaultTtl uint32,
 		} else { // DSL format:
 			switch hdr.Rrtype {
 			case dns.TypeMX:
-				mxi := strings.SplitN(target, "\t", 2)
-				target = mxi[0] + ", '" + mxi[1] + "'"
+				m := strings.SplitN(target, "\t", 2)
+				target = m[0] + ", '" + m[1] + "'"
 			case dns.TypeSOA:
 				continue
 			case dns.TypeTXT:
@@ -117,7 +120,7 @@ func rr_format(zonename string, filename string, r io.Reader, defaultTtl uint32,
 			default:
 				target = "'" + target + "'"
 			}
-			if hdr.Ttl == defaultTtl {
+			if hdr.Ttl == defaultTTL {
 				ttl = ""
 			} else {
 				ttl = fmt.Sprintf(", TTL(%d)", hdr.Ttl)
@@ -135,17 +138,17 @@ func main() {
 		flag.Usage()
 	}
 
-	defttl := uint32(*flag_defaultTtl)
+	defTTL := uint32(*flagDefaultTTL)
 
-	switch *output_mode {
+	switch *flagMode {
 	case "pretty":
-		pretty(zonename, filename, reader, defttl)
+		pretty(zonename, filename, reader, defTTL)
 	case "dsl":
-		fmt.Printf(`D("%s", %s, DnsProvider(%s)`, zonename, *flag_registrar, *flag_provider)
-		rr_format(zonename, filename, reader, defttl, true)
+		fmt.Printf(`D("%s", %s, DnsProvider(%s)`, zonename, *flagRegText, *flagProviderText)
+		rrFormat(zonename, filename, reader, defTTL, true)
 		fmt.Println("\n)")
 	case "tsv":
-		rr_format(zonename, filename, reader, defttl, false)
+		rrFormat(zonename, filename, reader, defTTL, false)
 	default:
 		flag.Usage()
 	}
