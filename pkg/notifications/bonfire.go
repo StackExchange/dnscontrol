0b329bf0eef0ea16302b4cde22dd04468aa5dce8
Craig Peterson <cpeterson@stackoverflow.com>
2018-01-11 12:44:52 -0500

    add emphasis

--- pkg/notifications/bonfire.go
+++ pkg/notifications/bonfire.go
@@ -25,7 +25,7 @@ func (b bonfireNotifier) Notify(domain, provider, msg string, err error, preview
 	} else if err != nil {
 		payload = fmt.Sprintf(`**ERROR running correction on %s[%s] -** (%s) Error: %s`, domain, provider, msg, err)
 	} else {
-		payload = fmt.Sprintf(`Successfully ran correction for %s[%s] - %s`, domain, provider, msg)
+		payload = fmt.Sprintf(`Successfully ran correction for **%s[%s]** - %s`, domain, provider, msg)
 	}
 	http.Post(string(b), "text/markdown", strings.NewReader(payload))
 }
