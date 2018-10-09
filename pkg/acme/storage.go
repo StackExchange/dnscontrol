1fd711080863793e42057d2382557f92ff4da6f0
Craig Peterson <192540+captncraig@users.noreply.github.com>
2018-10-09 18:51:48 -0400

    allow ecc certs if desired (#411)

--- pkg/acme/storage.go
+++ pkg/acme/storage.go
@@ -2,6 +2,7 @@ package acme
 
 import "github.com/xenolf/lego/acme"
 
+// Storage is an abstracrion around how certificates, keys, and account info are stored on disk or elsewhere.
 type Storage interface {
 	// Get Existing certificate, or return nil if it does not exist
 	GetCertificate(name string) (*acme.CertificateResource, error)
