f89a9d495353320a87656a0acdd997865f8d66fa
Tom Limoncelli <tlimoncelli@stackexchange.com>
2019-06-21 07:48:59 -0400

    Bugfix: redirect: should be redirect= (#507)

--- pkg/spflib/parse_test.go
+++ pkg/spflib/parse_test.go
@@ -42,17 +42,44 @@ func TestParseWithDoubleSpaces(t *testing.T) {
 }
 
 func TestParseRedirectNotLast(t *testing.T) {
-	// Make sure redirect:foo fails if it isn't the last item.
+	// Make sure redirect=foo fails if it isn't the last item.
+	dnsres, err := NewCache("testdata-dns1.json")
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = Parse(strings.Join([]string{"v=spf1",
+		"redirect=servers.mcsv.net",
+		"~all"}, " "), dnsres)
+	if err == nil {
+		t.Fatal("should fail")
+	}
+}
+
+func TestParseRedirectColon(t *testing.T) {
+	// Make sure redirect:foo fails.
 	dnsres, err := NewCache("testdata-dns1.json")
 	if err != nil {
 		t.Fatal(err)
 	}
 	_, err = Parse(strings.Join([]string{"v=spf1",
 		"redirect:servers.mcsv.net",
-		"~all"}, " "), dnsres)
+	}, " "), dnsres)
 	if err == nil {
 		t.Fatal("should fail")
 	}
+}
+
+func TestParseRedirectOnly(t *testing.T) {
+	dnsres, err := NewCache("testdata-dns1.json")
+	if err != nil {
+		t.Fatal(err)
+	}
+	rec, err := Parse(strings.Join([]string{"v=spf1",
+		"redirect=servers.mcsv.net"}, " "), dnsres)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(rec.Print())
 }
 
 func TestParseRedirectLast(t *testing.T) {
@@ -62,7 +89,7 @@ func TestParseRedirectLast(t *testing.T) {
 	}
 	rec, err := Parse(strings.Join([]string{"v=spf1",
 		"ip4:198.252.206.0/24",
-		"redirect:servers.mcsv.net"}, " "), dnsres)
+		"redirect=servers.mcsv.net"}, " "), dnsres)
 	if err != nil {
 		t.Fatal(err)
 	}
