0e78d4f4bc4ecdb105d603018a6203ecd009bd24
Craig Peterson <captncraig@users.noreply.github.com>
2019-06-27 01:29:13 -0400

    fix format again

--- providers/activedir/domains_test.go
+++ providers/activedir/domains_test.go
@@ -24,7 +24,7 @@ func TestGetExistingRecords(t *testing.T) {
 	}
 	expected := []*models.RecordConfig{
 		makeRC("@", "test2", "10.166.2.11", models.RecordConfig{Type: "A", TTL: 600}),
-		makeRC("_msdcs", "test2", "other_record", models.RecordConfig{Type: "NS", TTL: 300}), 
+		makeRC("_msdcs", "test2", "other_record", models.RecordConfig{Type: "NS", TTL: 300}),
 		makeRC("co-devsearch02", "test2", "10.8.2.64", models.RecordConfig{Type: "A", TTL: 3600}),
 		makeRC("co-devservice01", "test2", "10.8.2.48", models.RecordConfig{Type: "A", TTL: 1200}), // Downcased.
 		makeRC("yum", "test2", "10.8.0.59", models.RecordConfig{Type: "A", TTL: 3600}),
