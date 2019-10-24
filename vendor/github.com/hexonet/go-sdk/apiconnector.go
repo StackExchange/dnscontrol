2a0040292a194d8e2d42e4f55532835434357d71
Benoît Knecht <benoit.knecht@fsfe.org>
2019-10-24 16:50:42 +0200

    Use Go modules instead of vendor directory
    This commit is the result of
    
    ```
    go mod init github.com/StackExchange/dnscontrol
    git rm -r vendor
    go mod tidy
    ```

--- vendor/github.com/hexonet/go-sdk/apiconnector.go
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2018 Kai Schwarz (1API GmbH). All rights reserved.
-//
-// Use of this source code is governed by the MIT
-// license that can be found in the LICENSE.md file.
-
-package main
-
-func main() {
-}
--- vendor/github.com/vultr/govultr/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/vultr/govultr
-
-go 1.12
