43e9eec7c51385da793b00b6b91c62f7fa717b54
Tom Limoncelli <tlimoncelli@stackoverflow.com>
2019-12-20 13:27:56 -0500

    draft

---
+++ providers/gandi_v5/convert.go
@@ -0,0 +1,69 @@
+package gandi
+
+// Convert the provider's native record description to models.RecordConfig.
+
+import (
+	"github.com/StackExchange/dnscontrol/models"
+	"github.com/StackExchange/dnscontrol/pkg/printer"
+	"github.com/pkg/errors"
+	gandi "github.com/tiramiseb/go-gandi-livedns"
+)
+
+// nativeToRecord takes a DNS record from Gandi and returns our native RecordConfig format.
+func nativeToRecords(n gandi.ZoneRecord, origin string) (rcs []*models.RecordConfig) {
+
+	for _, value := range n.RrsetValues {
+
+		rc := &models.RecordConfig{
+			TTL:      uint32(n.RrsetTTL),
+			Original: n,
+		}
+		rc.SetLabel(n.RrsetName, origin)
+		switch rtype := n.RrsetType; rtype {
+		default: //  "A", "AAAA", "CAA", "NS", "CNAME", "MX", "PTR", "SRV", "TXT"
+			if err := rc.PopulateFromString(rtype, value, origin); err != nil {
+				panic(errors.Wrap(err, "unparsable record received from gandi"))
+			}
+		}
+		rcs = append(rcs, rc)
+	}
+
+	return rcs
+}
+
+func recordsToNative(rcs []*models.RecordConfig, origin string) []*gandi.ZoneRecord {
+
+	// Take a list of RecordConfig and return an equivalent list of
+	// ZoneRecords.  Gandi requires one ZoneRecord for each label:key tuple.
+
+	var keys = map[models.RecordKey]*gandi.ZoneRecord{}
+	var zrs []*gandi.ZoneRecord
+
+	for _, r := range rcs {
+		label := r.GetLabel()
+		key := r.Key()
+
+		if zr, ok := keys[key]; !ok {
+			// Allocate a new ZoneRecord:
+			zr := &gandi.ZoneRecord{
+				RrsetType:   r.Type,
+				RrsetTTL:    int(r.TTL),
+				RrsetName:   label,
+				RrsetValues: []string{r.GetTargetCombined()},
+			}
+			keys[key] = zr
+			zrs = append(zrs, zr)
+		} else {
+			// Update an existing ZoneRecord:
+			zr.RrsetValues = append(zr.RrsetValues, r.GetTargetCombined())
+			if r.TTL != uint32(zr.RrsetTTL) {
+				printer.Warnf("All TTLs for a rrset (%v) must be the same. Using smaller of %v and %v.\n", key, r.TTL, zr.RrsetTTL)
+				if r.TTL < uint32(zr.RrsetTTL) {
+					zr.RrsetTTL = int(r.TTL)
+				}
+			}
+		}
+	}
+
+	return zrs
+}
--- providers/gandi_v5/gandi_v5Provider.go
+++ providers/gandi_v5/gandi_v5Provider.go
@@ -2,8 +2,16 @@ package gandi
 
 import (
 	"encoding/json"
+	"fmt"
+	"strings"
 
+	"github.com/miekg/dns/dnsutil"
+	gandi "github.com/tiramiseb/go-gandi-livedns"
+
+	"github.com/StackExchange/dnscontrol/models"
+	"github.com/StackExchange/dnscontrol/pkg/printer"
 	"github.com/StackExchange/dnscontrol/providers"
+	"github.com/StackExchange/dnscontrol/providers/diff"
 	"github.com/pkg/errors"
 )
 
@@ -19,6 +27,15 @@ Info required in `creds.json`:
 
 */
 
+// Section 1: Register this provider in the system.
+
+// init registers the provider to dnscontrol.
+func init() {
+	providers.RegisterDomainServiceProviderType("GANDI_V5", newDsp, features)
+	providers.RegisterRegistrarType("GANDI_V5", newReg)
+}
+
+// features declares which features and options are available.
 var features = providers.DocumentationNotes{
 	providers.CanUseCAA:              providers.Can(),
 	providers.CanUsePTR:              providers.Can(),
@@ -28,65 +45,204 @@ var features = providers.DocumentationNotes{
 	providers.DocOfficiallySupported: providers.Cannot(),
 }
 
-func init() {
-	providers.RegisterDomainServiceProviderType("GANDI-V5", newDsp, features)
-	providers.RegisterRegistrarType("GANDI-V5", newReg)
+// Section 2: Define the API client.
+
+// gandiApi is the API handle used to store any client-related state.
+type gandiApi struct {
+	apikey    string
+	sharingid string
+	//	domainIndex map[string]int64 // Map of domainname to index
+	//	nameservers map[string][]*models.Nameserver
+	//	ZoneId      int64
 }
 
+// newDsp generates a DNS Service Provider client handle.
 func newDsp(conf map[string]string, metadata json.RawMessage) (providers.DNSServiceProvider, error) {
-	return newGandi(conf, metadata)
+	return newHelper(conf, metadata)
 }
 
+// newReg generates a Registrar Provider client handle.
 func newReg(conf map[string]string) (providers.Registrar, error) {
-	return newGandi(conf, nil)
+	return newHelper(conf, nil)
 }
 
-func newGandi(m map[string]string, metadata json.RawMessage) (*GandiApi, error) {
-	api := &GandiApi{}
-	api.ApiKey = m["apikey"]
-	if api.ApiKey == "" {
+// newHelper generates a handle handle.
+func newHelper(m map[string]string, metadata json.RawMessage) (*gandiApi, error) {
+	api := &gandiApi{}
+	api.apikey = m["apikey"]
+	api.sharingid = m["sharing_id"]
+	if api.apikey == "" {
 		return nil, errors.Errorf("missing Gandi apikey")
 	}
 
 	return api, nil
 }
 
-// api is the API handle for this module.
-type api struct {
-	//	ApiKey      string
-	//	domainIndex map[string]int64 // Map of domainname to index
-	//	nameservers map[string][]*models.Nameserver
-	//	ZoneId      int64
+// Section 3: DSP-related functions
+
+// GetNameservers returns a list of nameservers for domain.
+func (client *gandiApi) GetNameservers(domain string) ([]*models.Nameserver, error) {
+	g := gandi.New(client.apikey, client.sharingid)
+	nameservers, err := g.GetDomainNS(domain)
+	if err != nil {
+		return nil, err
+	}
+	return models.StringsToNameservers(nameservers), nil
 }
+
+func (client *gandiApi) GetDomainCorrections(dc *models.DomainConfig) ([]*models.Correction, error) {
+
+	existing, err := client.GetZoneRecords(dc.Name)
+	if err != nil {
+		return nil, err
+	}
+	models.PostProcessRecords(existing)
+	clean := PrepFoundRecods(existing)
+	PrepDesiredRecords(dc)
+	return client.GenerateDomainCorrections(dc, clean)
+}
+
+// GetZoneRecords gathers the DNS records and converts them to our
+// standard format.
+func (client *gandiApi) GetZoneRecords(domain string) ([]*models.RecordConfig, error) {
+	g := gandi.New(client.apikey, client.sharingid)
+	records, err := g.ListDomainRecords(domain)
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Printf("RECORDS: %+v\n", records)
+
+	// convert to dnscontrol RecordConfig format
+	existingRecords := []*models.RecordConfig{}
+	for _, rr := range records {
+		existingRecords = append(existingRecords, nativeToRecords(rr, domain)...)
+	}
+
+	return existingRecords, nil
+}
+
+func PrepFoundRecods(recs []*models.RecordConfig) []*models.RecordConfig {
+	return recs
+}
+
+func PrepDesiredRecords(dc *models.DomainConfig) {
+	// Sort through the dc.Records, eliminate any that can't be
+	// supported; modify any that need adjustments to work with the
+	// provider.  We try to do minimal changes otherwise it gets
+	// confusing.
+
+	dc.Punycode()
+
+	recordsToKeep := make([]*models.RecordConfig, 0, len(dc.Records))
+	for _, rec := range dc.Records {
+		if rec.TTL < 300 {
+			printer.Warnf("Gandi does not support ttls < 300. Setting %s from %d to 300\n", rec.GetLabelFQDN(), rec.TTL)
+			rec.TTL = 300
+		}
+		if rec.TTL > 2592000 {
+			printer.Warnf("Gandi does not support ttls > 30 days. Setting %s from %d to 2592000\n", rec.GetLabelFQDN(), rec.TTL)
+			rec.TTL = 2592000
+		}
+		if rec.Type == "TXT" {
+			rec.SetTarget("\"" + rec.GetTargetField() + "\"") // FIXME(tlim): Should do proper quoting.
+		}
+		if rec.Type == "NS" && rec.GetLabel() == "@" {
+			if !strings.HasSuffix(rec.GetTargetField(), ".gandi.net.") {
+				printer.Warnf("Gandi does not support changing apex NS records. Ignoring %s\n", rec.GetTargetField())
+			}
+			continue
+		}
+		recordsToKeep = append(recordsToKeep, rec)
+	}
+	dc.Records = recordsToKeep
+}
+
+func (client *gandiApi) GenerateDomainCorrections(dc *models.DomainConfig, existing []*models.RecordConfig) ([]*models.Correction, error) {
+
+	var corrections = []*models.Correction{}
+
+	// diff
+	differ := diff.New(dc, existing)
+	namesToUpdate := differ.ChangedGroups(existing)
+
+	if len(namesToUpdate) == 0 {
+		return nil, nil
+	}
+
+	// Gather all the RecordConfigs of any RecordKey (label+rtype) that needs updating.
+	updates := map[models.RecordKey][]*models.RecordConfig{}
+	for k := range namesToUpdate {
+		updates[k] = nil
+		for _, rc := range dc.Records {
+			if rc.Key() == k {
+				updates[k] = append(updates[k], rc)
+			}
+		}
+	}
+
+	g := gandi.New(client.apikey, client.sharingid)
+
+	// For any key with an update, delete or replace those records.
+	for k, recs := range updates {
+		if len(recs) == 0 {
+			// No records matching this key?  This can only mean that all
+			// the records were deleted. Delete them.
+			corrections = append(corrections,
+				&models.Correction{
+					Msg: strings.Join(namesToUpdate[k], "\n"),
+					F: func() error {
+						shortname := dnsutil.TrimDomainName(k.NameFQDN, dc.Name)
+						err := g.DeleteDomainRecord(dc.Name, shortname, k.Type)
+						if err != nil {
+							return err
+						}
+						return nil
+					},
+				})
+
+		} else {
+			// Convert all the records to a single ZoneRecord.  The
+			// RrsetValues should be a list of all the targets for this key.
+			ns := recordsToNative(recs, dc.Name)
+			if len(ns) != 1 {
+				panic(errors.Error("Should not happen. Keys should have collapsed down to one ZoneRecord."))
+			}
+
+			corrections = append(corrections,
+				&models.Correction{
+					Msg: strings.Join(namesToUpdate[k], "\n"),
+					F: func() error {
+						res, err := g.ChangeDomainRecordWithNameAndType(dc.Name, recs[0].GetLabel(), recs[0].Type, recs[0].TTL, ns[0].RrsetValues)
+						if err != nil {
+							return errors.Wrapf(err, "%+v", res)
+						}
+						return nil
+					},
+				})
+		}
+	}
+}
+
+/*
+
+
+	If a key has zero updates, it is a delete.
+	How are we going to do deletes?
+		DeleteDomainRecords(fqdn, name string) (err error)
+
+	If it has any changes (it is non-zero), then we are lazy and just replace all the records for that label.
+	We could be fancy and update the exact records, but this is
+
+	ChangeDomainRecordsWithName
+
+*/
 
 //type gandiRecord struct {
 //	gandirecord.RecordInfo
 //}
 
-func (c *api) GetNameservers(domain string) ([]*Nameserver, error) {
-	// // GetNameservers returns the nameservers for domain.
-	// func (c *GandiApi) GetNameservers(domain string) ([]*models.Nameserver, error) {
-	// 	domaininfo, err := c.getDomainInfo(domain)
-	// 	if err != nil {
-	// 		return nil, err
-	// 	}
-	// 	ns := []*models.Nameserver{}
-	// 	for _, nsname := range domaininfo.Nameservers {
-	// 		ns = append(ns, &models.Nameserver{Name: nsname})
-	// 	}
-	// 	return ns, nil
-	// }
-}
-
-func GetDomainCorrections(dc *DomainConfig) ([]*Correction, error) {
-
-	// call GetZoneRecords()
-	// call CleanZoneRecords()
-	// call GenerateZoneCorrections()
-
-}
-
-// func (c *GandiApi) getDomainInfo(domain string) (*gandidomain.DomainInfo, error) {
+// func (c *gandiApi) getDomainInfo(domain string) (*gandidomain.DomainInfo, error) {
 // 	if err := c.fetchDomainList(); err != nil {
 // 		return nil, err
 // 	}
@@ -98,7 +254,7 @@ func GetDomainCorrections(dc *DomainConfig) ([]*Correction, error) {
 // }
 //
 // // GetNameservers returns the nameservers for domain.
-// func (c *GandiApi) GetNameservers(domain string) ([]*models.Nameserver, error) {
+// func (c *gandiApi) GetNameservers(domain string) ([]*models.Nameserver, error) {
 // 	domaininfo, err := c.getDomainInfo(domain)
 // 	if err != nil {
 // 		return nil, err
@@ -111,7 +267,7 @@ func GetDomainCorrections(dc *DomainConfig) ([]*Correction, error) {
 // }
 //
 // // GetDomainCorrections returns a list of corrections recommended for this domain.
-// func (c *GandiApi) GetDomainCorrections(dc *models.DomainConfig) ([]*models.Correction, error) {
+// func (c *gandiApi) GetDomainCorrections(dc *models.DomainConfig) ([]*models.Correction, error) {
 // 	dc.Punycode()
 // 	domaininfo, err := c.getDomainInfo(dc.Name)
 // 	if err != nil {
@@ -189,30 +345,30 @@ func GetDomainCorrections(dc *DomainConfig) ([]*Correction, error) {
 //
 // 	return corrections, nil
 // }
-//
-// // GetRegistrarCorrections returns a list of corrections for this registrar.
-// func (c *GandiApi) GetRegistrarCorrections(dc *models.DomainConfig) ([]*models.Correction, error) {
-// 	domaininfo, err := c.getDomainInfo(dc.Name)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	sort.Strings(domaininfo.Nameservers)
-// 	found := strings.Join(domaininfo.Nameservers, ",")
-// 	desiredNs := []string{}
-// 	for _, d := range dc.Nameservers {
-// 		desiredNs = append(desiredNs, d.Name)
-// 	}
-// 	sort.Strings(desiredNs)
-// 	desired := strings.Join(desiredNs, ",")
-// 	if found != desired {
-// 		return []*models.Correction{
-// 			{
-// 				Msg: fmt.Sprintf("Change Nameservers from '%s' to '%s'", found, desired),
-// 				F: func() (err error) {
-// 					_, err = c.setDomainNameservers(dc.Name, desiredNs)
-// 					return
-// 				}},
-// 		}, nil
-// 	}
-// 	return nil, nil
-// }
+
+// GetRegistrarCorrections returns a list of corrections for this registrar.
+func (c *gandiApi) GetRegistrarCorrections(dc *models.DomainConfig) ([]*models.Correction, error) {
+	// 	domaininfo, err := c.getDomainInfo(dc.Name)
+	// 	if err != nil {
+	// 		return nil, err
+	// 	}
+	// 	sort.Strings(domaininfo.Nameservers)
+	// 	found := strings.Join(domaininfo.Nameservers, ",")
+	// 	desiredNs := []string{}
+	// 	for _, d := range dc.Nameservers {
+	// 		desiredNs = append(desiredNs, d.Name)
+	// 	}
+	// 	sort.Strings(desiredNs)
+	// 	desired := strings.Join(desiredNs, ",")
+	// 	if found != desired {
+	// 		return []*models.Correction{
+	// 			{
+	// 				Msg: fmt.Sprintf("Change Nameservers from '%s' to '%s'", found, desired),
+	// 				F: func() (err error) {
+	// 					_, err = c.setDomainNameservers(dc.Name, desiredNs)
+	// 					return
+	// 				}},
+	// 		}, nil
+	// 	}
+	return nil, nil
+}
