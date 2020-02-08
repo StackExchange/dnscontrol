c4869fee6c7a8b677aafa8988b6ac1e0f37ca3ab
Tom Limoncelli <tlimoncelli@stackoverflow.com>
2020-02-07 20:38:53 -0500

    WIP!

--- commands/getZone.go
+++ commands/getZone.go
@@ -2,9 +2,16 @@ package commands
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
 
 	"github.com/StackExchange/dnscontrol/v2/providers"
+	"github.com/StackExchange/dnscontrol/v2/providers/bind"
 	"github.com/StackExchange/dnscontrol/v2/providers/config"
+	"github.com/miekg/dns"
+	"github.com/miekg/dns/dnsutil"
 	"github.com/urfave/cli/v2"
 )
 
@@ -70,6 +77,8 @@ func GetZone(args GetZoneArgs) error {
 	var providerConfigs map[string]map[string]string
 	var err error
 
+	// Read it in:
+
 	providerConfigs, err = config.LoadProviderConfigs(args.CredsFile)
 	if err != nil {
 		return err
@@ -81,16 +90,105 @@ func GetZone(args GetZoneArgs) error {
 	fmt.Printf("cred = %v\n", args.CredName)
 	fmt.Printf("prov = %v\n", args.ProviderName)
 	fmt.Printf("zone = %v\n", args.ZoneName)
+	zonename := args.ZoneName
 
 	provider, err := providers.CreateDNSProvider(args.ProviderName, providerConfigs[args.CredName], nil)
 	if err != nil {
 		return err
 	}
-	fmt.Printf("FOO=%+v\n", provider)
+	//fmt.Printf("FOO=%+v\n", provider)
 
-	recs, err := provider.GetZoneRecords(args.ZoneName)
-	fmt.Printf("RECS=%v\n", recs)
-	fmt.Printf("err = %v\n", err)
+	recs, err := provider.GetZoneRecords(zonename)
+	//fmt.Printf("RECS=%v\n", recs)
+	//fmt.Printf("err = %v\n", err)
+	if err != nil {
+		return err
+	}
 
-	return err
+	// Write it out:
+
+	switch args.OutputFile {
+	case "pretty":
+		bind.WriteZoneFile(os.Stdout, recs, zonename)
+	case "dsl":
+		fmt.Printf(`var CHANGEME = NewDnsProvider("%s", "%s");`+"\n",
+			args.CredName, args.ProviderName)
+		fmt.Printf(`D("%s", REG_CHANGEME,`+"\n", zonename)
+		fmt.Printf(`  DnsProvider(CHANGEME),` + "\n")
+		rrFormat(zonename, filename, recs, defTTL, true)
+		fmt.Println("\n)")
+	case "tsv":
+		rrFormat(zonename, filename, recs, defTTL, false)
+	default:
+		return fmt.Errorf("format %q unknown", args.OutputFile)
+	}
+
+	return nil
+}
+
+// rrFormat outputs the zonefile in either DSL or TSV format.
+func rrFormat(zonename string, filename string, recs []dns.RR, defaultTTL uint32, dsl bool) {
+	zonenamedot := zonename + "."
+
+	for _, x := range recs {
+
+		// Skip comments. Parse the formatted version.
+		line := x.String()
+		if line[0] == ';' {
+			continue
+		}
+		items := strings.SplitN(line, "\t", 5)
+		if len(items) < 5 {
+			log.Fatalf("Too few items in: %v", line)
+		}
+
+		target := items[4]
+
+		hdr := x.Header()
+		nameFqdn := hdr.Name
+		name := dnsutil.TrimDomainName(nameFqdn, zonenamedot)
+		ttl := strconv.FormatUint(uint64(hdr.Ttl), 10)
+		classStr := dns.ClassToString[hdr.Class]
+		typeStr := dns.TypeToString[hdr.Rrtype]
+
+		// MX records should split out the prio vs. target.
+		if hdr.Rrtype == dns.TypeMX {
+			target = strings.Replace(target, " ", "\t", 1)
+		}
+
+		var ttlop string
+		if hdr.Ttl == defaultTTL {
+			ttlop = ""
+		} else {
+			ttlop = fmt.Sprintf(", TTL(%d)", hdr.Ttl)
+		}
+
+		// NS records at the apex should be NAMESERVER() records.
+		if hdr.Rrtype == dns.TypeNS && name == "@" {
+			fmt.Printf(",\n\tNAMESERVER('%s'%s)", target, ttlop)
+			continue
+		}
+
+		if !dsl { // TSV format:
+			fmt.Printf("%s\t%s\t%s\t%s\t%s\n", name, ttl, classStr, typeStr, target)
+		} else { // DSL format:
+			switch hdr.Rrtype { // #rtype_variations
+			case dns.TypeMX:
+				m := strings.SplitN(target, "\t", 2)
+				target = m[0] + ", '" + m[1] + "'"
+			case dns.TypeSOA:
+				continue
+			case dns.TypeTXT:
+				if len(x.(*dns.TXT).Txt) == 1 {
+					target = `'` + x.(*dns.TXT).Txt[0] + `'`
+				} else {
+					target = `['` + strings.Join(x.(*dns.TXT).Txt, `', '`) + `']`
+				}
+			default:
+				target = "'" + target + "'"
+			}
+			fmt.Printf(",\n\t%s('%s', %s%s)", typeStr, name, target, ttlop)
+		}
+	}
+
 }
