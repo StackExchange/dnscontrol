508d58118b32c5ac3dd6234dd98c2fcf423cf6fc
Tom Limoncelli <tlimoncelli@stackoverflow.com>
2020-03-09 12:34:41 -0400

    wip!

--- commands/gz_test.go
+++ commands/gz_test.go
@@ -1,6 +1,7 @@
 package commands
 
 import (
+	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
@@ -17,7 +18,7 @@ This file does a number of tests:
 
 // Basic conversions:        OUTPUTFILE:        EXPECTED FILE:
   example.org.zone -> tsv      $NAME.tsv          *.expected
-  example.org.zone -> dsl      $NAME.dsl          *.expected
+  example.org.zone -> jsd      $NAME.jsd          *.expected
   example.org.zone -> zonefile $NAME.zon          *.expected
 
 // Loop test:
@@ -26,13 +27,22 @@ This file does a number of tests:
 
 */
 
-func TestZoneToZone(t *testing.T) {
+func TestFormatTypes(t *testing.T) {
+	for i, domainname := range []string{"simple.com", "example.org"} {
+		t.Run(fmt.Sprintf("%s/tsv", domain), func(t *testing.T) { testFormat(t, domainname, "tsv") })
+		t.Run(fmt.Sprintf("%s/zon", domain), func(t *testing.T) { testFormat(t, domainname, "zon") })
+		t.Run(fmt.Sprintf("%s/js", domain), func(t *testing.T) { testFormat(t, domainname, "js") })
+	}
+}
 
-	// Compute: test_data/example.org.zone + creds.json ==> tempfile
-	// Compare: tempfile to test_data/expected_zonefile.txt
+func testFormat(t *testing.T, domainname, format string) {
+	t.Helper()
 
-	// Create temp file for output.
-	outfile, err := ioutil.TempFile("", "example.*.txt")
+	sourceFilename := fmt.Sprintf("test_data/%s.zone", domainname)
+	expectedFilename := fmt.Sprintf("test_data/%s.zone.%s", domainname, format)
+	outputFiletmpl := fmt.Sprintf("%s.zone.%s.*.txt", domainname, format)
+
+	outfile, err := ioutil.TempFile("", outputFiletmpl)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -40,36 +50,40 @@ func TestZoneToZone(t *testing.T) {
 
 	// Convert test data to the experiment output.
 	gzargs := GetZoneArgs{
+		ZoneNames:    []string{domainname}
+		OutputFormat: format,
+		OutputFile:   outfile.Name(),
 		CredName:     "bind",
 		ProviderName: "BIND",
-		ZoneNames:    []string{"example.org"},
-		OutputFormat: "pretty",
-		OutputFile:   outfile.Name(),
-		//DefaultTTL:   0,
 	}
 	gzargs.CredsFile = "test_data/creds.json"
+
+	// Read the zonefile and convert
 	err = GetZone(gzargs)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	want, err := ioutil.ReadFile("test_data/expected_zonefile.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
-
+	// Read the actual result:
 	got, err := ioutil.ReadFile(outfile.Name())
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	//err = ioutil.WriteFile("test_data/expected_zonefile.txt", got, 0644)
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
+	// Read the expected result
+	want, err := ioutil.ReadFile(expectedFilename)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Copy got -> want
+	err = ioutil.WriteFile(expectedFilename, got, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	if diff := cmp.Diff(want, got); diff != "" {
-		t.Errorf("TestZoneToZone() mismatch (-want +got):\n%s", diff)
+		t.Errorf("TestFormatTypes mismatch (-want +got):\n%s", diff)
 	}
 
 }
