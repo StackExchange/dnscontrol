9a3f13e99df70cfc8005f89269b1e1f338bbad13
Tom Limoncelli <tlimoncelli@stackoverflow.com>
2020-05-30 10:50:03 -0400

    DS: More cleanups

--- integrationTest/integration_test.go
+++ integrationTest/integration_test.go
@@ -373,11 +373,11 @@ func naptr(name string, order uint16, preference uint16, flags string, service s
 	return r
 }
 
-func ds(name string, keyTag uint16, algorithm, digestType uint8, digest string) *rec {
+func ds(name string, keytag uint16, algorithm, digesttype uint8, digest string) *rec {
 	r := makeRec(name, "", "DS")
-	r.DsKeyTag = keyTag
+	r.DsKeyTag = keytag
 	r.DsAlgorithm = algorithm
-	r.DsDigestType = digestType
+	r.DsDigestType = digesttype
 	r.DsDigest = digest
 	return r
 }
