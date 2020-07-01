a5c33f98d24700afa60b3c20de5f425b617ccbd4
Tom Limoncelli <tlimoncelli@stackoverflow.com>
2020-07-01 05:55:20 -0400

    Sort updates (cosmetic) (#772)
    * Sort updates (cosmetic)
    
    * comments
    
    * More comments

--- providers/desec/desecProvider.go
+++ providers/desec/desecProvider.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"sort"
 
 	"github.com/StackExchange/dnscontrol/v3/models"
 	"github.com/StackExchange/dnscontrol/v3/pkg/diff"
@@ -214,6 +215,14 @@ func (c *api) GenerateDomainCorrections(dc *models.DomainConfig, existing models
 				return nil
 			},
 		})
+
+	// NB(tlim): This sort is just to make updates look pretty. It is
+	// cosmetic.  The risk here is that there may be some updates that
+	// require a specific order (for example a delete before an add).
+	// However the code doesn't seem to have such situation.  All tests
+	// pass.  That said, if this breaks anything, the easiest fix might
+	// be to just remove the sort.
+	sort.Slice(corrections, func(i, j int) bool { return diff.CorrectionLess(corrections, i, j) })
 
 	return corrections, nil
 }
