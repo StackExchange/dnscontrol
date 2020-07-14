df69016cd1c11e6ca67bfaa2cc368b20c58ef3d2
Tom Limoncelli <tlimoncelli@stackoverflow.com>
2020-07-14 07:20:07 -0400

    No-op change

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ func main() {
 	os.Exit(commands.Run(versionString()))
 }
 
-// Version management. 2 Goals:
+// Version management. Goals:
 // 1. Someone who just does "go get" has at least some information.
 // 2. If built with build/build.go, more specific build information gets put in.
 // Update the number here manually each release, so at least we have a range for go-get people.
