f0086c52c07bc8c3cbe883a44a84ba4a688c8535
Tom Limoncelli <tlimoncelli@stackoverflow.com>
2020-11-18 07:59:09 -0500

    fixup!

--- providers/gcloud/gcloudProvider.go
+++ providers/gcloud/gcloudProvider.go
@@ -260,52 +260,6 @@ func (g *gcloudProvider) GetDomainCorrections(dc *models.DomainConfig) ([]*model
 	}}, nil
 }
 
-// backoff is the amount of time to sleep if a 429 or 504 is received.
-// It is doubled after each use.
-var backoff = time.Second * 5
-
-const maxBackoff = time.Minute * 3
-
-func retryNeeded(resp *gdns.Change, err error) bool {
-	if err != nil {
-		return false // Not an error.
-	}
-	//log.Printf("RUNCHANGE RESP = %+v\n", resp)
-	//log.Printf("RUNCHANGE ERR = %v\n", err)
-	//log.Printf("RUNCHANGE ERR+ = %+v\n", err)
-	serr, ok := err.(*googleapi.Error)
-	if !ok {
-		return false // Not a google error.
-	}
-	if serr.Code == 200 {
-		return false // Success! No need to pause.
-		// TODO(tlim): Consider resetting backoff here?
-	}
-	if serr.Code != 404 && serr.Code != 429 && serr.Code != 503 {
-		return false // Not an error we care about.
-	}
-
-	// TODO(tlim): In theory, resp.Header has a header that says how
-	// long to wait but I haven't been able to capture that header in
-	// the wild. If you get these "RUNCHANGE HEAD" messages, please
-	// file a bug with the contents!
-
-	if resp != nil {
-		log.Printf("RUNCHANGE CODE = %+v\n", resp.HTTPStatusCode)
-		log.Printf("RUNCHANGE HEAD = %+v\n", resp.Header)
-	}
-
-	// a simple exponential back-off with a 3-minute max.
-	log.Printf("Pausing due to ratelimit: %v seconds\n", backoff)
-	time.Sleep(backoff)
-	backoff = backoff + (backoff / 2)
-	if backoff > maxBackoff {
-		backoff = maxBackoff
-	}
-
-	return true
-}
-
 func nativeToRecord(set *gdns.ResourceRecordSet, rec, origin string) (*models.RecordConfig, error) {
 	r := &models.RecordConfig{}
 	r.SetLabelFromFQDN(set.Name, origin)
@@ -375,4 +329,67 @@ func (g *gcloudProvider) EnsureDomainExists(domain string) error {
 	g.zones = nil // reset cache
 	_, err = g.client.ManagedZones.Create(g.project, mz).Do()
 	return err
+}
+
+const initialBackoff = time.Second * 10 // First delay duration
+const maxBackoff = time.Minute * 3      // Maximum backoff delay
+
+// backoff is the amount of time to sleep if a 429 or 504 is received.
+// It is doubled after each use.
+var backoff = initialBackoff
+var backoff404 = false // Set if the last call requested a retry of a 404
+
+func retryNeeded(resp *gdns.Change, err error) bool {
+	if err != nil {
+		return false // Not an error.
+	}
+	serr, ok := err.(*googleapi.Error)
+	if !ok {
+		return false // Not a google error.
+	}
+	if serr.Code == 200 {
+		backoff = initialBackoff // Reset
+		return false             // Success! No need to retry.
+	}
+
+	if serr.Code == 404 {
+		// serr.Code == 404 happens occasionally when GCLOUD hasn't
+		// finished updating the database yet.  We pause and retry
+		// exactly once. There should be a better way to do this, such as
+		// a callback that would tell us a transaction is complete.
+		if backoff404 {
+			backoff404 = false
+			return false // Give up. We've done this already.
+		}
+		log.Printf("Special 404 pause-and-retry for GCLOUD: Pausing %s\n", backoff)
+		time.Sleep(backoff)
+		backoff404 = true
+		return true // Request a retry.
+	}
+	backoff404 = false
+
+	if serr.Code != 429 && serr.Code != 503 {
+		return false // Not an error that permits retrying.
+	}
+
+	// TODO(tlim): In theory, resp.Header has a header that says how
+	// long to wait but I haven't been able to capture that header in
+	// the wild. If you get these "RUNCHANGE HEAD" messages, please
+	// file a bug with the contents!
+
+	if resp != nil {
+		log.Printf("NOTE: If you see this message, please file a bug with the output below:\n")
+		log.Printf("RUNCHANGE CODE = %+v\n", resp.HTTPStatusCode)
+		log.Printf("RUNCHANGE HEAD = %+v\n", resp.Header)
+	}
+
+	// a simple exponential back-off
+	log.Printf("Pausing due to ratelimit: %v seconds\n", backoff)
+	time.Sleep(backoff)
+	backoff = backoff + (backoff / 2)
+	if backoff > maxBackoff {
+		backoff = maxBackoff
+	}
+
+	return true // Request the API call be re-tried.
 }
