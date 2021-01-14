df7be5866b9fc5cbe9bd53d9f27af2cc3066c13d
Tom Limoncelli <tlimoncelli@stackoverflow.com>
2021-01-14 09:39:05 -0500

    CLOUDFLARE: Now works with PunyCode/IDNA

--- providers/cloudflare/cloudflareProvider.go
+++ providers/cloudflare/cloudflareProvider.go
@@ -149,6 +149,11 @@ func (c *cloudflareProvider) getDomainID(name string) (string, error) {
 
 // GetDomainCorrections returns a list of corrections to update a domain.
 func (c *cloudflareProvider) GetDomainCorrections(dc *models.DomainConfig) ([]*models.Correction, error) {
+	err := dc.Punycode()
+	if err != nil {
+		return nil, err
+	}
+
 	id, err := c.getDomainID(dc.Name)
 	if err != nil {
 		return nil, err
