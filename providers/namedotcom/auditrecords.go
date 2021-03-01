2698a82d117c5d9d7f9bf5b6d1ce33560ac63400
Tom Limoncelli <tlimoncelli@stackoverflow.com>
2021-02-28 20:12:47 -0500

    fix NDC

--- providers/namedotcom/auditrecords.go
+++ providers/namedotcom/auditrecords.go
@@ -1,6 +1,8 @@
 package namedotcom
 
 import (
+	"fmt"
+
 	"github.com/StackExchange/dnscontrol/v3/models"
 	"github.com/StackExchange/dnscontrol/v3/pkg/recordaudit"
 )
@@ -8,6 +10,11 @@ import (
 // AuditRecords returns an error if any records are not
 // supportable by this provider.
 func AuditRecords(records []*models.RecordConfig) error {
+
+
+	if err := MaxLengthNDC(records); err != nil {
+		return err
+	}
 
 	if err := recordaudit.TxtNoMultipleStrings(records); err != nil {
 		return err
@@ -19,3 +26,25 @@ func AuditRecords(records []*models.RecordConfig) error {
 
 	return nil
 }
+
+
+
+
+// MaxLengthNDC returns and error if the sum of the strings
+// are longer than permitted by DigitalOcean. Sadly their
+// length limit is undocumented. This seems to work.
+func MaxLengthNDC(records []*models.RecordConfig) error {
+	for _, rc := range records {
+
+		if rc.HasFormatIdenticalToTXT() { // TXT and similar:
+			if len(rc.GetTargetField()) > 509 {
+				return fmt.Errorf("encoded txt too long")
+			}
+		}
+
+	}
+	return nil
+}
+
+
+
