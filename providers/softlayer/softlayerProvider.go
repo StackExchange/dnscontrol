08636614c15b4cd76d0a11083343cab141efd6a2
Tom Limoncelli <tlimoncelli@stackoverflow.com>
2021-03-04 20:10:47 -0500

    Warn about Softlayer

--- providers/softlayer/softlayerProvider.go
+++ providers/softlayer/softlayerProvider.go
@@ -31,6 +31,7 @@ func init() {
 }
 
 func newReg(conf map[string]string, _ json.RawMessage) (providers.DNSServiceProvider, error) {
+	fmt.Println("WARNING: THe SOFTLAYER provider is unmaintained. https://github.com/StackExchange/dnscontrol/issues/1079")
 	s := session.New(conf["username"], conf["api_key"], conf["endpoint_url"], conf["timeout"])
 
 	if len(s.UserName) == 0 || len(s.APIKey) == 0 {
