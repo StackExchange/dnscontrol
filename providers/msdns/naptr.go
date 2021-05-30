eac93f101be6af2c6abc9f73d713346bf9a920a4
Tom Limoncelli <tlimoncelli@stackoverflow.com>
2021-05-30 15:46:56 -0400

    !wip

--- providers/msdns/naptr.go
+++ providers/msdns/naptr.go
@@ -5,7 +5,10 @@ package msdns
 
 import (
 	"bytes"
+	"encoding/hex"
 	"fmt"
+	"log"
+	"strconv"
 
 	"github.com/StackExchange/dnscontrol/v3/models"
 )
@@ -22,4 +25,49 @@ func generatePSCCreateNaptr(domain string, rec *models.RecordConfig) string {
 	fmt.Fprintf(&b, `$Replacement = '%s'\n`, rec.GetTargetField())
 	fmt.Fprintf(&b, `dnscmd /recordadd $zoneName $rrName naptr $Order $Preference $Flags $Service $Regex $Replacement\n`)
 	return b.String()
+}
+
+func decodeRecordDataNaptr(s string) models.RecordConfig {
+	// C8AFB0B30153075349502B4432540474657374165F7369702E5F7463702E6578616D706C652E6F72672E
+	rc := models.RecordConfig{}
+
+	s, rc.NaptrOrder = eatUint16(s)
+	s, rc.NaptrPreference = eatUint16(s)
+	s, rc.NaptrFlags = eatString(s)
+	s, rc.NaptrService = eatString(s)
+	s, rc.NaptrRegexp = eatString(s)
+	s, targ := eatString(s)
+	rc.SetTarget(targ)
+	if s != "" {
+		fmt.Printf("WARNING: REMAINDER:=%q\n", s)
+	}
+
+	return rc
+}
+
+func eatUint16(s string) (string, uint16) {
+	value, err := strconv.ParseUint(s[2:4]+s[0:2], 16, 64)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return s[4:], uint16(value)
+}
+
+func eatString(s string) (string, string) {
+	sl, err := strconv.ParseUint(s[:2], 16, 64)
+	if err != nil {
+		log.Fatal(err)
+	}
+	last := 2 + sl*2
+	hexcoded := s[2:last]
+	ret, err := hex.DecodeString(hexcoded)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Decoding: %s\n", s)
+	fmt.Printf("      sl: %d\n", sl)
+	fmt.Printf("    last: %d\n", last)
+	fmt.Printf("     ret: %q\n", ret)
+	return s[last:], string(ret)
 }
