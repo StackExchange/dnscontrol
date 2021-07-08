228b57e445e18349cd0d9d100005c487884d29ee
Georg <georg@neuland.tech>
2021-07-08 16:06:54 +0200

    DESEC: Implements support for long / multistring txt records (#1204)
    * use /auth/account endpoint for token validation
    this implements the token validation using the /auth/account api endpoint as suggested in #1177 instead of fetching the domain list
    
    * deSEC: add support for long txt records #996
    
    * deSEC: add support for a different api error response
    relates to #996 where we had insufficient error output due to unknown api error format
    
    * deSEC: remove unused fetchDomainList function
    
    * deSEC: improve error handling
    
    * deSEC: support for long / multistring txt records
    the previous commit was broken this is now working (CRUD)
    
    * deSEC: document what desecProvider.domainIndex is used for
    
    * deSEC: handle the rate limiting correctly
    we try to use the Retry-After header to determine how long we should sleep until retry
    
    * deSEC: further improvement of rate limit handling
    we cut off if the Retry-After header exceeds 3 minutes because this might be the daily limit.
    
    Co-authored-by: Tom Limoncelli <tlimoncelli@stackoverflow.com>

--- providers/desec/convert.go
+++ providers/desec/convert.go
@@ -4,7 +4,6 @@ package desec
 
 import (
 	"fmt"
-	"strings"
 
 	"github.com/StackExchange/dnscontrol/v3/models"
 	"github.com/StackExchange/dnscontrol/v3/pkg/printer"
@@ -25,9 +24,7 @@ func nativeToRecords(n resourceRecord, origin string) (rcs []*models.RecordConfi
 		}
 		rc.SetLabel(n.Subname, origin)
 		switch rtype := n.Type; rtype {
-		case "TXT":
-			rc.SetTargetTXT(value)
-		default: //  "A", "AAAA", "CAA", "NS", "CNAME", "MX", "PTR", "SRV"
+		default: //  "A", "AAAA", "CAA", "NS", "CNAME", "MX", "PTR", "SRV", "TXT"
 			if err := rc.PopulateFromString(rtype, value, origin); err != nil {
 				panic(fmt.Errorf("unparsable record received from deSEC: %w", err))
 			}
@@ -45,7 +42,6 @@ func recordsToNative(rcs []*models.RecordConfig, origin string) []resourceRecord
 
 	var keys = map[models.RecordKey]*resourceRecord{}
 	var zrs []resourceRecord
-
 	for _, r := range rcs {
 		label := r.GetLabel()
 		if label == "@" {
@@ -60,9 +56,6 @@ func recordsToNative(rcs []*models.RecordConfig, origin string) []resourceRecord
 				TTL:     r.TTL,
 				Subname: label,
 				Records: []string{r.GetTargetCombined()},
-			}
-			if r.Type == "TXT" {
-				zr.Records = []string{strings.Join(r.TxtStrings, "")}
 			}
 			zrs = append(zrs, zr)
 			//keys[key] = &zr   // This didn't work.
