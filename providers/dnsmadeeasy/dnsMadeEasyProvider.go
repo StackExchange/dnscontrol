StackExchange/dnscontrol

commit c22293e2cf2df5373cf7b0d76126e13cf36406ae

Correct hard coded DNS Made Easy system nameserver TTL (#1167)
In my (very limited) experience with DNS Made Easy, the system nameserver
TTL is always 86400 and is not able to be altered.

Without this change, if you specify any TTL (other than 300) via
NAMESERVER_TTL() dnscontrol detects a difference and attempts to update
DNS Made Easy with the new value, which fails because the system
nameservers cannot be altered.

With this change in place the same thing will obviously still happen, if
you use NAMESERVER_TTL() with any TTL other than 86400, but that will be
a bit less confusing since the 86400 value appears in the DNS Made Easy
UI at least.

providers/dnsmadeeasy/types.go
@@ -188,6 +188,9 @@ func fromRecordConfig(rc *models.RecordConfig) *recordRequestData {
 }
 
 func systemNameServerToRecordConfig(domain string, nameServer string) *models.RecordConfig {
+	// DNS Made Easy does not allow the system name servers to be edited, and said records appear to always have a fixed TTL of 86400.
+	const fixedNameServerRecordTTL = 86400;
+
 	target := nameServer + "."
-	return toRecordConfig(domain, &recordResponseDataEntry{Type: "NS", Value: target, TTL: int(models.DefaultTTL)})
+	return toRecordConfig(domain, &recordResponseDataEntry{Type: "NS", Value: target, TTL: fixedNameServerRecordTTL})
 }

commit 346667e03eea77427c23e62b2887d13f77ce4e0e

DNSMADEEASY: always override NS records TTL to be 86400
Fixes broken integration tests and spamming output about chaning NS records TTL from fixed one (86400) to dnscontrol default one (300).

This issue was introduced in #1167.

providers/dnsmadeeasy/dnsMadeEasyProvider.go
@@ -73,10 +73,13 @@ func (api *dnsMadeEasyProvider) GetDomainCorrections(dc *models.DomainConfig) ([
 		return nil, err
 	}
 
-	// ALIAS is called ANAME on DNS Made Easy
 	for _, rec := range dc.Records {
 		if rec.Type == "ALIAS" {
+			// ALIAS is called ANAME on DNS Made Easy
 			rec.Type = "ANAME"
+		} else if rec.Type == "NS" {
+			// NS records have fixed TTL on DNS Made Easy and it cannot be changed
+			rec.TTL = fixedNameServerRecordTTL
 		}
 	}
 

providers/dnsmadeeasy/types.go
@@ -6,6 +6,9 @@ import (
 	"github.com/StackExchange/dnscontrol/v3/models"
 )
 
+// DNS Made Easy does not allow the system name servers to be edited, and said records appear to always have a fixed TTL of 86400.
+const fixedNameServerRecordTTL = 86400
+
 type singleDomainResponse struct {
 	ID                  int                              `json:"id"`
 	Name                string                           `json:"name"`
@@ -188,9 +191,6 @@ func fromRecordConfig(rc *models.RecordConfig) *recordRequestData {
 }
 
 func systemNameServerToRecordConfig(domain string, nameServer string) *models.RecordConfig {
-	// DNS Made Easy does not allow the system name servers to be edited, and said records appear to always have a fixed TTL of 86400.
-	const fixedNameServerRecordTTL = 86400
-
 	target := nameServer + "."
 	return toRecordConfig(domain, &recordResponseDataEntry{Type: "NS", Value: target, TTL: fixedNameServerRecordTTL})
 }