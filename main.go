015c3d184adc8f0f8a62e69e44d8b83d0d5da5e1
Tom Limoncelli <tlimoncelli@stackoverflow.com>
2022-06-19 08:39:56 -0400

    Release v3.17.0

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ import (
 // Update the number here manually each release, so at least we have a range for go-get people.
 var (
 	SHA       = ""
-	Version   = "v3.16.2"
+	Version   = "v3.17.0"
 	BuildTime = ""
 )
 
