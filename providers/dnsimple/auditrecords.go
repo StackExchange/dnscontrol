0bfd587ce233dc3bd10020ee0c1afeabdbe065ca
Amelia Aronsohn <squirrel@wearing.black>
2022-07-21 20:14:07 -0700

    Add some commentary

--- providers/dnsimple/auditrecords.go
+++ providers/dnsimple/auditrecords.go
@@ -8,21 +8,22 @@ import (
 // AuditRecords returns an error if any records are not
 // supportable by this provider.
 func AuditRecords(records []*models.RecordConfig) error {
+	//TODO(onlyhavecans) I think we can support multiple strings.
 	if err := recordaudit.TxtNoMultipleStrings(records); err != nil {
 		return err
 	}
 
 	if err := recordaudit.TxtNoTrailingSpace(records); err != nil {
 		return err
-	}
+	} // as of 2022-07
 
 	if err := recordaudit.TxtNotEmpty(records); err != nil {
 		return err
-	}
+	} // as of 2022-07
 
 	if err := recordaudit.TxtNoUnpairedDoubleQuotes(records); err != nil {
 		return err
-	}
+	} // as of 2022-07
 
 	return nil
 }
