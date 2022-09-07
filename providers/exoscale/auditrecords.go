61e500b7dc0ffa5039ca7fb62abca9c446a02778
Tom Limoncelli <tlimoncelli@stackoverflow.com>
2022-09-07 14:08:06 -0400

    DEV: Optimize integration tests (#1742)

--- providers/exoscale/auditrecords.go
+++ providers/exoscale/auditrecords.go
@@ -1,10 +1,21 @@
 package exoscale
 
-import "github.com/StackExchange/dnscontrol/v3/models"
+import (
+	"github.com/StackExchange/dnscontrol/v3/models"
+	"github.com/StackExchange/dnscontrol/v3/pkg/rejectif"
+)
 
 // AuditRecords returns a list of errors corresponding to the records
 // that aren't supported by this provider.  If all records are
 // supported, an empty list is returned.
 func AuditRecords(records []*models.RecordConfig) []error {
-	return nil
+	a := rejectif.Auditor{}
+
+	a.Add("CAA", rejectif.CaaTargetContainsWhitespace) // Last verified 2022-07-11
+
+	a.Add("MX", rejectif.MxNull) // Last verified 2022-07-11
+
+	a.Add("SRV", rejectif.SrvHasNullTarget) // Last verified 2020-12-28
+
+	return a.Audit(records)
 }
--- providers/hexonet/auditrecords.go
+++ providers/hexonet/auditrecords.go
@@ -13,5 +13,7 @@ func AuditRecords(records []*models.RecordConfig) []error {
 
 	a.Add("TXT", rejectif.TxtIsEmpty) // Last verified 2021-10-01
 
+	a.Add("SRV", rejectif.SrvHasNullTarget) // Last verified 2020-12-28
+
 	return a.Audit(records)
 }
