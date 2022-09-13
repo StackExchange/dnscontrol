94b7e843d25d0ccee160b06c534d0687fda22677
Brian Hartvigsen <brian.andrew@brianandjenny.com>
2022-09-13 12:59:58 -0700

    chore(DigitalOcean): Support semicolon as CAA target
    This appears to be a change as it is supported in the interface and in the API. Verified with my own personal domains (e.g. brianandjenny.net)

--- providers/digitalocean/auditrecords.go
+++ providers/digitalocean/auditrecords.go
@@ -15,8 +15,6 @@ func AuditRecords(records []*models.RecordConfig) []error {
 
 	a.Add("CAA", rejectif.CaaTargetContainsWhitespace) // Last verified xxxx-xx-xx
 
-	a.Add("CAA", rejectif.CaaTargetHasSemicolon) // Last verified 2021-03-01
-
 	a.Add("MX", rejectif.MxNull) // Last verified 2020-12-28
 
 	a.Add("TXT", MaxLengthDO) // Last verified 2021-03-01
