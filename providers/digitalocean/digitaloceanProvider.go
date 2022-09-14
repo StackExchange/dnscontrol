49590df8bf491908093d68d7ac4c9a5a3de10c37
Brian Hartvigsen <brian.andrew@brianandjenny.com>
2022-09-14 04:18:53 -0700

    DIGITALOCEAN: semicolon as CAA target is now supported (#1749)

--- providers/digitalocean/digitaloceanProvider.go
+++ providers/digitalocean/digitaloceanProvider.go
@@ -72,7 +72,7 @@ retry:
 
 var features = providers.DocumentationNotes{
 	providers.CanGetZones:            providers.Can(),
-	providers.CanUseCAA:              providers.Can("Semicolons not supported in issue/issuewild fields.", "https://www.digitalocean.com/docs/networking/dns/how-to/create-caa-records"),
+	providers.CanUseCAA:              providers.Can(),
 	providers.CanUseSRV:              providers.Can(),
 	providers.DocCreateDomains:       providers.Can(),
 	providers.DocOfficiallySupported: providers.Cannot(),
