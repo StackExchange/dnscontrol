fad3447646ad9759495fedbd23f716eacc8f4329
Jeffrey Cafferata <jeffrey@jcid.nl>
2022-11-01 20:22:35 +0100

    Included the gitbook.io DNS CNAME-record.

--- .gitbook/domains/dnscontrol.zone.js
+++ .gitbook/domains/dnscontrol.zone.js
@@ -3,5 +3,6 @@ D('dnscontrol.zone',
   DnsProvider(PROVIDER_TRANSIP),
   DefaultTTL(3600),
   TXT('@', 'v=spf1 -all'),
-  TXT('_dmarc', 'v=DMARC1; p=none;')
+  TXT('_dmarc', 'v=DMARC1; p=none;'),
+  CNAME('docs', 'db3053e25d-hosting.gitbook.io.')
 )
