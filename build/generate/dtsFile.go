9b3ad81b1d15d5dac75a4ffef4d2481f066cb5ba
Jed Fox <git@jedfox.com>
2023-01-17 12:10:43 -0500

    Embed types-dnscontrol.d.ts into the binary instead of fetching it via HTTP (#1942)
    Co-authored-by: Tom Limoncelli <tlimoncelli@stackoverflow.com>

--- build/generate/dtsFile.go
+++ build/generate/dtsFile.go
@@ -16,13 +16,13 @@ func generateDTSFile(funcs string) error {
 		"// WARNING: These type definitions are experimental and subject to change in future releases.",
 	}
 	for _, name := range names {
-		content, err := os.ReadFile(join("types", "src", name+".d.ts"))
+		content, err := os.ReadFile(join("commands", "types", name+".d.ts"))
 		if err != nil {
 			return err
 		}
 		combined = append(combined, string(content))
 	}
 	combined = append(combined, funcs)
-	os.WriteFile(join("types", "dnscontrol.d.ts"), []byte(strings.Join(combined, "\n\n")), 0644)
+	os.WriteFile(join("commands", "types", "dnscontrol.d.ts"), []byte(strings.Join(combined, "\n\n")), 0644)
 	return nil
 }
