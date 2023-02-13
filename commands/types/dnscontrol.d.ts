6edbf454b7ff1eb8258df5ef65a957e834e8da14
Jed Fox <git@jedfox.com>
2023-02-13 13:38:30 -0500

    Update SOA doc comment

--- commands/types/dnscontrol.d.ts
+++ commands/types/dnscontrol.d.ts
@@ -1032,12 +1032,14 @@ declare function R53_ALIAS(name: string, target: string, zone_idModifier: Domain
  * 
  * ```javascript
  * D("example.com", REG_THIRDPARTY, DnsProvider("DNS_BIND"),
- *   SOA("@", "ns3.example.org.", "hostmaster.example.org.", 3600, 600, 604800, 1440),
+ *   SOA("@", "ns3.example.org.", "hostmaster@example.org", 3600, 600, 604800, 1440),
  * );
  * ```
  * 
- * ## Notes
+ * The email address should be specified like a normal RFC822/RFC5322 address (user@hostname.com). It will be converted into the required format (e.g. BIND format: `user.hostname.com`) by the provider as required. This has the benefit of being more human-readable plus DNSControl can properly handle escaping and other issues.
  * 
+ * ## Notes
+ * * Previously, the accepted format for the SOA mailbox field was `hostmaster.example.org`. This has been changed to `hostmaster@example.org`
  * * The serial number is managed automatically.  It isn't even a field in `SOA()`.
  * * Most providers automatically generate SOA records.  They will ignore any `SOA()` statements.
  * 
