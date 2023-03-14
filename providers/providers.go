85b29a398bfd6d6daf568ebeff01bd1ca7d74d54
Tom Limoncelli <tlimoncelli@stackoverflow.com>
2023-03-14 18:01:29 -0400

    fix NONE

--- providers/providers.go
+++ providers/providers.go
@@ -158,18 +158,12 @@ func (n None) GetNameservers(string) ([]*models.Nameserver, error) {
 
 // GetZoneRecords gets the records of a zone and returns them in RecordConfig format.
 func (n None) GetZoneRecords(domain string) (models.Records, error) {
-	return nil, fmt.Errorf("not implemented")
-	// This enables the get-zones subcommand.
-	// Implement this by extracting the code from GetDomainCorrections into
-	// a single function.  For most providers this should be relatively easy.
+	return nil, nil
 }
 
 // GetZoneRecordsCorrections gets the records of a zone and returns them in RecordConfig format.
 func (n None) GetZoneRecordsCorrections(dc *models.DomainConfig, records models.Records) ([]*models.Correction, error) {
-	return nil, fmt.Errorf("not implemented")
-	// This enables the get-zones subcommand.
-	// Implement this by extracting the code from GetDomainCorrections into
-	// a single function.  For most providers this should be relatively easy.
+	return nil, nil
 }
 
 // GetDomainCorrections returns corrections to update a domain.
