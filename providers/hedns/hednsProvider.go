dc6599bf314dc63974f7c35809f41b0797b04dda
Tom Limoncelli <tlimoncelli@stackoverflow.com>
2023-03-16 19:45:31 -0400

    HE.dns works

--- providers/hedns/hednsProvider.go
+++ providers/hedns/hednsProvider.go
@@ -49,7 +49,7 @@ var features = providers.DocumentationNotes{
 	providers.CanUseCAA:              providers.Can(),
 	providers.CanUseDS:               providers.Cannot(),
 	providers.CanUseDSForChildren:    providers.Cannot(),
-	providers.CanUseLOC:              providers.Unimplemented(),
+	providers.CanUseLOC:              providers.Can(),
 	providers.CanUseNAPTR:            providers.Can(),
 	providers.CanUsePTR:              providers.Can(),
 	providers.CanUseSOA:              providers.Cannot(),
