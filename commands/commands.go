3d1cd30a1debced4b68449a638c09abeb1bb8b7c
Tom Limoncelli <tlimoncelli@stackoverflow.com>
2023-05-12 10:52:31 -0400

    BUG: "dnscontrol version" not working (#2356)
    Co-authored-by: Tom Limoncelli <tal@whatexit.org>

--- commands/commands.go
+++ commands/commands.go
@@ -22,7 +22,13 @@ const (
 )
 
 var commands = []*cli.Command{}
-var version string
+
+// These are set by/for goreleaser
+var (
+	version = "dev"
+	commit  = "none"
+	date    = "unknown"
+)
 
 func cmd(cat string, c *cli.Command) bool {
 	c.Category = cat
