c75a280c98f3dace21d140e0ecf0e981c2af2475
Tom Limoncelli <tlimoncelli@stackoverflow.com>
2023-05-23 12:45:10 -0400

    remove redundant test 042

--- pkg/js/parse_tests/042-unmanaged.js
+++
@@ -1,9 +0,0 @@
-D("foo.com", "none"
-  , IGNORE("", "", "targetGlob1")
-  , IGNORE("", "CNAME", "")
-  , IGNORE("", "A", "targetGlob3")
-  , IGNORE("lab4")
-  , IGNORE("notype", "", "targetGlob5")
-  , IGNORE("lab6", "A, CNAME")
-  , IGNORE("lab7", "TXT", "targetGlob7")
-);
