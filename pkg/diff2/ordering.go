956f130b043effccd081ae4db75c6b9db7efb697
Vincent Hagen <vinnie@script4web.nl>
2023-06-20 23:14:37 +0200

    Improved informational messages

--- pkg/diff2/ordering.go
+++ pkg/diff2/ordering.go
@@ -11,7 +11,11 @@ func orderByDependencies(changes ChangeList) ChangeList {
 	a := dnssort.SortUsingGraph(changes)
 
 	if len(a.UnresolvedRecords) > 0 {
-		log.Printf("Found unresolved records %v\n", dnsgraph.GetRecordsNamesForGraphables(a.UnresolvedRecords))
+		log.Printf("Found unresolved records %v.\n"+
+			"This can indicate a circular dependency, please ensure all targets from given records exist and no circular dependencies exist in the changeset."+
+			"These unresolved records are still added as changes and pushed to the provider, but will cause issues if and when the provider checks the changes\n",
+			dnsgraph.GetRecordsNamesForGraphables(a.UnresolvedRecords),
+		)
 	}
 
 	return a.SortedRecords
