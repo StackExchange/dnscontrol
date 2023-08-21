d4f6e01e7a6b2516e3f4a264012f83761ee6a80a
Tom Limoncelli <tlimoncelli@stackoverflow.com>
2023-08-21 16:16:13 -0400

    "get zones" js/djs format should generate double quotes, not single (#2526)

--- commands/r53_test.go
+++ commands/r53_test.go
@@ -16,7 +16,7 @@ func TestR53Test_1(t *testing.T) {
 	rec.SetTarget("bar")
 	rec.R53Alias = make(map[string]string)
 	rec.R53Alias["type"] = "A"
-	w := `R53_ALIAS('foo', 'A', 'bar')`
+	w := `R53_ALIAS("foo", "A", "bar")`
 	if g := makeR53alias(&rec, 0); g != w {
 		t.Errorf("makeR53alias failure: got `%s` want `%s`", g, w)
 	}
@@ -31,7 +31,7 @@ func TestR53Test_1ttl(t *testing.T) {
 	rec.SetTarget("bar")
 	rec.R53Alias = make(map[string]string)
 	rec.R53Alias["type"] = "A"
-	w := `R53_ALIAS('foo', 'A', 'bar', TTL(321))`
+	w := `R53_ALIAS("foo", "A", "bar", TTL(321))`
 	if g := makeR53alias(&rec, 321); g != w {
 		t.Errorf("makeR53alias failure: got `%s` want `%s`", g, w)
 	}
@@ -47,7 +47,7 @@ func TestR53Test_2(t *testing.T) {
 	rec.R53Alias = make(map[string]string)
 	rec.R53Alias["type"] = "A"
 	rec.R53Alias["zone_id"] = "blarg"
-	w := `R53_ALIAS('foo', 'A', 'bar', R53_ZONE('blarg'))`
+	w := `R53_ALIAS("foo", "A", "bar", R53_ZONE("blarg"))`
 	if g := makeR53alias(&rec, 0); g != w {
 		t.Errorf("makeR53alias failure: got `%s` want `%s`", g, w)
 	}
@@ -63,7 +63,7 @@ func TestR53Test_2ttl(t *testing.T) {
 	rec.R53Alias = make(map[string]string)
 	rec.R53Alias["type"] = "A"
 	rec.R53Alias["zone_id"] = "blarg"
-	w := `R53_ALIAS('foo', 'A', 'bar', R53_ZONE('blarg'), TTL(123))`
+	w := `R53_ALIAS("foo", "A", "bar", R53_ZONE("blarg"), TTL(123))`
 	if g := makeR53alias(&rec, 123); g != w {
 		t.Errorf("makeR53alias failure: got `%s` want `%s`", g, w)
 	}
