0af3fea3d4017d25f124fec536874211700cc275
Costas Drogos <costas.drogos@gmail.com>
2023-08-25 22:07:40 +0300

    Fix tests

--- pkg/notifications/notifications_test.go
+++ pkg/notifications/notifications_test.go
@@ -7,10 +7,7 @@ func Test_stripAnsiColorsValid(t *testing.T) {
 	coloredStr := "\x1b[0133myellow\x1b[0m" // 33 == yellow
 	nonColoredStr := "yellow"
 
-	s, err := stripAnsiColors(coloredStr)
-	if err != nil {
-		t.Errorf("stripAnsiColors() failed to compile ansi regex")
-	}
+	s := stripAnsiColors(coloredStr)
 	if s != nonColoredStr {
 		t.Errorf("stripAnsiColors() stripped %q different from %q", coloredStr, nonColoredStr)
 	}
@@ -21,10 +18,7 @@ func Test_stripAnsiColorsInvalid(t *testing.T) {
 	coloredStr := "\x1b[01AAmyellow\x1b[0m" // AA not a real color
 	nonColoredStr := "yellow"
 
-	s, err := stripAnsiColors(coloredStr)
-	if err != nil {
-		t.Errorf("stripAnsiColors() failed to compile ansi regex")
-	}
+	s := stripAnsiColors(coloredStr)
 	if s == nonColoredStr {
 		t.Errorf("stripAnsiColors() stripped %q should be different from %q", coloredStr, nonColoredStr)
 	}
