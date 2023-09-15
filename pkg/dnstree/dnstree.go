2bfed3ddf6ae63e420d81e42e5497479103ccf37
Tom Limoncelli <tlimoncelli@stackoverflow.com>
2023-09-15 14:59:58 -0400

    golint recommends x-- instead of x -= 1

--- pkg/dnstree/dnstree.go
+++ pkg/dnstree/dnstree.go
@@ -17,6 +17,7 @@ func Create[T any]() *DomainTree[T] {
 	}
 }
 
+// DomainTree is a domain tree.
 type DomainTree[T any] domainNode[T]
 
 type domainNode[T any] struct {
@@ -47,7 +48,7 @@ func (tree *DomainTree[T]) Set(fqdn string, data T) {
 	}
 
 	ptr := (*domainNode[T])(tree)
-	for iX := len(domainParts) - 1; iX > 0; iX -= 1 {
+	for iX := len(domainParts) - 1; iX > 0; iX-- {
 		ptr = ptr.addIntermediate(domainParts[iX])
 	}
 
@@ -69,7 +70,7 @@ func (tree *DomainTree[T]) Get(fqdn string) T {
 	var mostSpecificNode *domainNode[T]
 	ptr := (*domainNode[T])(tree)
 
-	for iX := len(domainParts) - 1; iX >= 0; iX -= 1 {
+	for iX := len(domainParts) - 1; iX >= 0; iX-- {
 		node, ok := ptr.Children[domainParts[iX]]
 		if !ok {
 			if mostSpecificNode != nil {
@@ -103,7 +104,7 @@ func (tree *DomainTree[T]) Has(fqdn string) bool {
 	var mostSpecificNode *domainNode[T]
 	ptr := (*domainNode[T])(tree)
 
-	for iX := len(domainParts) - 1; iX >= 0; iX -= 1 {
+	for iX := len(domainParts) - 1; iX >= 0; iX-- {
 		node, ok := ptr.Children[domainParts[iX]]
 		if !ok {
 			return mostSpecificNode != nil
