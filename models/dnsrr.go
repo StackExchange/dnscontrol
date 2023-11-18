cdc4cd17e0c69fa3202747b98f98877db74b4007
Tom Limoncelli <tlimoncelli@stackoverflow.com>
2023-11-18 09:30:01 -0500

    Remove commented-out Header code

--- models/dnsrr.go
+++ models/dnsrr.go
@@ -9,34 +9,10 @@ import (
 	"github.com/miekg/dns"
 )
 
-//// Header Header returns the header of an resource record.
-//func (rc *RecordConfig) Header() *dns.RR_Header {
-//	log.Fatal("Header not implemented")
-//	return nil
-//}
-
 // String returns the text representation of the resource record.
 func (rc *RecordConfig) String() string {
 	return rc.GetTargetCombined()
 }
-
-//// copy returns a copy of the RR
-//func (rc *RecordConfig) copy() dns.RR {
-//	log.Fatal("Copy not implemented")
-//	return dns.TypeToRR[dns.TypeA]()
-//}
-//
-//// len returns the length (in octets) of the uncompressed RR in wire format.
-//func (rc *RecordConfig) len() int {
-//	log.Fatal("len not implemented")
-//	return 0
-//}
-//
-//// pack packs an RR into wire format.
-//func (rc *RecordConfig) pack([]byte, int, map[string]int, bool) (int, error) {
-//	log.Fatal("pack not implemented")
-//	return 0, nil
-//}
 
 // Conversions
 
