f5069f8dda0b98d0b95a32f80e9b0c9c665a32ac
Tom Limoncelli <tlimoncelli@stackoverflow.com>
2023-11-18 10:03:59 -0500

    Enable debugWarnTxtField

--- models/target.go
+++ models/target.go
@@ -3,6 +3,7 @@ package models
 import (
 	"fmt"
 	"net"
+	"runtime/debug"
 	"strings"
 
 	"github.com/miekg/dns"
@@ -18,7 +19,7 @@ Not the best design, but we're stuck with it until we re-do RecordConfig, possib
 // GetTargetField works fine on TXT records for casual output but it
 // is often better to access .TxtStrings directly or call
 // GetTargetRFC1035Quoted() for nicely quoted text.
-var debugWarnTxtField = false
+var debugWarnTxtField = true
 
 // GetTargetField returns the target. There may be other fields (for example
 // an MX record also has a .MxPreference field.
@@ -26,7 +27,7 @@ func (rc *RecordConfig) GetTargetField() string {
 	if debugWarnTxtField {
 		if rc.Type == "TXT" {
 			fmt.Printf("DEBUG: WARNING: GetTargetField called on TXT record is frequently wrong: %q\n", rc.target)
-			//debug.PrintStack()
+			debug.PrintStack()
 		}
 	}
 	return rc.target
