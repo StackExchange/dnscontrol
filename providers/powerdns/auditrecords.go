d1a63472ff604afa0dceaaac0afaad319f30bada
Tom Limoncelli <tlimoncelli@stackoverflow.com>
2023-11-19 13:18:03 -0500

    cleanups

--- providers/powerdns/auditrecords.go
+++ providers/powerdns/auditrecords.go
@@ -6,5 +6,10 @@ import "github.com/StackExchange/dnscontrol/v4/models"
 // that aren't supported by this provider.  If all records are
 // supported, an empty list is returned.
 func AuditRecords(records []*models.RecordConfig) []error {
-	return nil
+	a := rejectif.Auditor{}
+
+	a.Add("TXT", rejectif.TxtHasDoubleQuotes) // Last verified 2023-11-11
+	a.Add("TXT", rejectif.TxtHasBackslash)    // Last verified 2023-11-11
+
+	return a.Audit(records)
 }
