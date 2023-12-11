790513a17063dc82f88c30a0c46052542e3f0a41
Tom Limoncelli <tlimoncelli@stackoverflow.com>
2023-12-11 16:24:11 -0500

    CHORE: Fix golint and staticcheck errors/warnings (#2717)

--- pkg/dnssort/result.go
+++ pkg/dnssort/result.go
@@ -2,7 +2,10 @@ package dnssort
 
 import "github.com/StackExchange/dnscontrol/v4/pkg/dnsgraph"
 
+// SortResult is the result of a sort function.
 type SortResult[T dnsgraph.Graphable] struct {
-	SortedRecords     []T
+	// SortedRecords is the sorted records.
+	SortedRecords []T
+	// UnresolvedRecords is the records that could not be resolved.
 	UnresolvedRecords []T
 }
