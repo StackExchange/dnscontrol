a34e2594fc6acbd4f4ce9ba6bb0be1df8e6540e4
Tom Limoncelli <tlimoncelli@stackoverflow.com>
2024-01-08 17:06:46 -0500

    gofmt

--- providers/realtimeregister/auditrecords.go
+++ providers/realtimeregister/auditrecords.go
@@ -5,6 +5,8 @@ import (
 	"github.com/StackExchange/dnscontrol/v4/pkg/rejectif"
 )
 
+
+
 // AuditRecords returns a list of errors corresponding to the records
 // that aren't supported by this provider.  If all records are
 // supported, an empty list is returned.
@@ -12,8 +14,8 @@ func AuditRecords(records []*models.RecordConfig) []error {
 	auditor := rejectif.Auditor{}
 
 	auditor.Add("TXT", rejectif.TxtHasTrailingSpace) // Last verified 2024-01-03
-	
-	auditor.Add("TXT", rejectif.TxtIsEmpty)   // Last verified 2024-01-03
+
+	auditor.Add("TXT", rejectif.TxtIsEmpty) // Last verified 2024-01-03
 
 	return auditor.Audit(records)
 }
