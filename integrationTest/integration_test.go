fab3172fee1880b7df316706fb76eacd669a60e8
Tom Limoncelli <tlimoncelli@stackoverflow.com>
2024-02-08 16:39:30 -0500

    CHORE: Refactor integration tests to support multiple tests

--- integrationTest/integration_test.go
+++ integrationTest/integration_test.go
@@ -276,8 +276,12 @@ func makeChanges(t *testing.T, prv providers.DNSServiceProvider, dc *models.Doma
 }
 
 func runTests(t *testing.T, prv providers.DNSServiceProvider, domainName string, knownFailures map[int]bool, origConfig map[string]string) {
-	dc := getDomainConfigWithNameservers(t, prv, domainName)
 	testGroups := makeTests(t)
+	runTestsHelper(t, prv, domainName, testGroups, origConfig)
+}
+
+func runTestsHelper(t *testing.T, prv providers.DNSServiceProvider, domainName string, testGroups []*TestGroup, origConfig map[string]string) {
+	dc := getDomainConfigWithNameservers(t, prv, domainName)
 
 	firstGroup := *startIdx
 	if firstGroup == -1 {
