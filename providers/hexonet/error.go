107b585d79c7c288c70310b56337b2e37d70ac33
Asif Nawaz <107853964+AsifNawaz-cnic@users.noreply.github.com>
2024-05-22 16:41:07 +0100

    HEXONET: Adopt version 4.0.0 of the rtldev-middleware-go-sdk (#2974)

--- providers/hexonet/error.go
+++ providers/hexonet/error.go
@@ -3,7 +3,7 @@ package hexonet
 import (
 	"fmt"
 
-	"github.com/centralnicgroup-opensource/rtldev-middleware-go-sdk/v3/response"
+	"github.com/centralnicgroup-opensource/rtldev-middleware-go-sdk/v4/response"
 )
 
 // GetHXApiError returns an error including API error code and error description.
--- providers/hexonet/hexonetProvider.go
+++ providers/hexonet/hexonetProvider.go
@@ -6,7 +6,7 @@ import (
 	"fmt"
 
 	"github.com/StackExchange/dnscontrol/v4/providers"
-	hxcl "github.com/centralnicgroup-opensource/rtldev-middleware-go-sdk/v3/apiclient"
+	hxcl "github.com/centralnicgroup-opensource/rtldev-middleware-go-sdk/v4/apiclient"
 )
 
 // GoReleaser: version
