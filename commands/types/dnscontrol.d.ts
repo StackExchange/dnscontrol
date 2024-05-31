c782c2eeca91e70855f7233dfd036f46a1af61e8
Jeffrey Cafferata <jeffrey@jcid.nl>
2024-05-31 22:57:33 +0200

    DOCS: Added links to `AUTODNSSEC_*` and applied code highlights.

--- commands/types/dnscontrol.d.ts
+++ commands/types/dnscontrol.d.ts
@@ -247,24 +247,24 @@ declare function AKAMAICDN(name: string, target: string, ...modifiers: RecordMod
 declare function ALIAS(name: string, target: string, ...modifiers: RecordModifier[]): DomainModifier;
 
 /**
- * AUTODNSSEC_OFF tells the provider to disable AutoDNSSEC. It takes no
+ * `AUTODNSSEC_OFF` tells the provider to disable AutoDNSSEC. It takes no
  * parameters.
  *
- * See `AUTODNSSEC_ON` for further details.
+ * See [`AUTODNSSEC_ON`](AUTODNSSEC_ON.md) for further details.
  *
  * @see https://docs.dnscontrol.org/language-reference/domain-modifiers/autodnssec_off
  */
 declare const AUTODNSSEC_OFF: DomainModifier;
 
 /**
- * AUTODNSSEC_ON tells the provider to enable AutoDNSSEC.
+ * `AUTODNSSEC_ON` tells the provider to **enable** AutoDNSSEC.
  *
- * AUTODNSSEC_OFF tells the provider to disable AutoDNSSEC.
+ * [`AUTODNSSEC_OFF`](AUTODNSSEC_OFF.md) tells the provider to **disable** AutoDNSSEC.
  *
  * AutoDNSSEC is a feature where a DNS provider can automatically manage
  * DNSSEC for a domain. Not all providers support this.
  *
- * At this time, AUTODNSSEC_ON takes no parameters.  There is no ability
+ * At this time, `AUTODNSSEC_ON` takes no parameters.  There is no ability
  * to tune what the DNS provider sets, no algorithm choice.  We simply
  * ask that they follow their defaults when enabling a no-fuss DNSSEC
  * data model.
