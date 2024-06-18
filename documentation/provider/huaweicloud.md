2d15884eb3da65f17533037f1bf79e3f8a57f9cb
Hui Hui <github@huihui.contact>
2024-06-19 04:17:02 +0800

    HUAWEICLOUD: add metadata to control Intelligent Resolution (#3013)

--- documentation/provider/huaweicloud.md
+++ documentation/provider/huaweicloud.md
@@ -20,7 +20,37 @@ Example:
 {% endcode %}
 
 ## Metadata
-This provider does not recognize any special metadata fields unique to Huawei Cloud DNS.
+There are some record level metadata available for this provider:
+   * `hw_line` (Line ID, default "default_view") Refer to the [Intelligent Resolution](https://support.huaweicloud.com/intl/en-us/usermanual-dns/dns_usermanual_0041.html) for more information.
+     * Available Line ID refer to [Resolution Lines](https://support.huaweicloud.com/intl/en-us/api-dns/en-us_topic_0085546214.html). Custom Line ID can also be used.
+   * `hw_weight` (0-1000, default "1") Refer to the [Configuring Weighted Routing](https://support.huaweicloud.com/intl/en-us/usermanual-dns/dns_usermanual_0705.html) for more information.
+   * `hw_rrset_key` (default "") User defined key for RRset load balance. This value would be stored in the description field of the RRset.
+
+The following example shows how to use the metadata:
+
+{% code title="dnsconfig.js" %}
+```javascript
+var REG_NONE = NewRegistrar("none");
+var DSP_HWCLOUD = NewDnsProvider("huaweicloud");
+
+D("example.com", REG_NONE, DnsProvider(DSP_HWCLOUD),
+    // this example will create 4 rrsets with the same name "test"
+    A("test", "8.8.8.8"),
+    A("test", "8.8.4.4"),
+    A("test", "9.9.9.9", {hw_weight: "10"}),         // Weighted Routing
+    A("test", "149.112.112.112", {hw_weight: "10"}), // Weighted Routing
+    A("test", "223.5.5.5", {hw_line: "CN"}), // GEODNS
+    A("test", "223.6.6.6", {hw_line: "CN", hw_weight: "10"}), // GEODNS with weight
+
+    // this example will create 3 rrsets with the same name "lb"
+    A("rr-lb", "10.0.0.1", {hw_weight: "10", hw_rrset_key: "lb-zone-a"}),
+    A("rr-lb", "10.0.0.2", {hw_weight: "10", hw_rrset_key: "lb-zone-a"}),
+    A("rr-lb", "10.0.1.1", {hw_weight: "10", hw_rrset_key: "lb-zone-b"}),
+    A("rr-lb", "10.0.1.2", {hw_weight: "10", hw_rrset_key: "lb-zone-b"}),
+    A("rr-lb", "10.0.2.2", {hw_weight: "0",  hw_rrset_key: "lb-zone-c"}),
+END);
+```
+{% endcode %}
 
 ## Usage
 An example configuration:
@@ -71,7 +101,3 @@ If that doesn't work, log into Huaweicloud's website and open the [API Explorer]
 
 ## New domains
 If a domain does not exist in your Huawei Cloud account, DNSControl will automatically add it with the `push` command.
-
-## GeoDNS
-Managing GeoDNS RRSet on Huawei Cloud (also called **Line** in Huawei Cloud DNS) is not supported in DNSControl.
-If your Zone needs to use GeoDNS, please create it manually in the console and use [IGNORE](../language-reference/domain-modifiers/IGNORE.md) modifiers in DNSControl to prevent changing it.
--- providers/huaweicloud/convert.go
+++ providers/huaweicloud/convert.go
@@ -3,6 +3,7 @@ package huaweicloud
 import (
 	"fmt"
 	"slices"
+	"strconv"
 
 	"github.com/StackExchange/dnscontrol/v4/models"
 	"github.com/StackExchange/dnscontrol/v4/pkg/printer"
@@ -15,16 +16,16 @@ func getRRSetIDFromRecords(rcs models.Records) []string {
 		if r.Original == nil {
 			continue
 		}
-		if r.Original.(*model.ListRecordSets).Id == nil {
+		if r.Original.(*model.ShowRecordSetByZoneResp).Id == nil {
 			printer.Warnf("RecordSet ID is nil for record %+v\n", r)
 			continue
 		}
-		ids = append(ids, *r.Original.(*model.ListRecordSets).Id)
+		ids = append(ids, *r.Original.(*model.ShowRecordSetByZoneResp).Id)
 	}
 	return slices.Compact(ids)
 }
 
-func nativeToRecords(n *model.ListRecordSets, zoneName string) (models.Records, error) {
+func nativeToRecords(n *model.ShowRecordSetByZoneResp, zoneName string) (models.Records, error) {
 	if n.Name == nil || n.Type == nil || n.Records == nil || n.Ttl == nil {
 		return nil, fmt.Errorf("missing required fields in Huaweicloud's RRset: %+v", n)
 	}
@@ -37,10 +38,20 @@ func nativeToRecords(n *model.ListRecordSets, zoneName string) (models.Records, 
 		rc := &models.RecordConfig{
 			TTL:      uint32(*n.Ttl),
 			Original: n,
+			Metadata: map[string]string{},
 		}
 		rc.SetLabelFromFQDN(recName, zoneName)
 		if err := rc.PopulateFromString(recType, value, zoneName); err != nil {
 			return nil, fmt.Errorf("unparsable record received from Huaweicloud: %w", err)
+		}
+		if n.Line != nil {
+			rc.Metadata[metaLine] = *n.Line
+		}
+		if n.Weight != nil {
+			rc.Metadata[metaWeight] = fmt.Sprintf("%d", *n.Weight)
+		}
+		if n.Description != nil {
+			rc.Metadata[metaKey] = *n.Description
 		}
 		rcs = append(rcs, rc)
 	}
@@ -48,15 +59,50 @@ func nativeToRecords(n *model.ListRecordSets, zoneName string) (models.Records, 
 	return rcs, nil
 }
 
-func recordsToNative(rcs models.Records, expectedKey models.RecordKey) *model.ListRecordSets {
+func recordsToNative(rcs models.Records, expectedKey models.RecordKey) (*model.ShowRecordSetByZoneResp, error) {
+	// rcs length is guaranteed to be > 0
+	if len(rcs) == 0 {
+		return nil, fmt.Errorf("empty record set")
+	}
+	// line and weight should be the same for all records in the rrset
+	line := rcs[0].Metadata[metaLine]
+	weightStr := rcs[0].Metadata[metaWeight]
+	for _, r := range rcs {
+		if r.Metadata[metaLine] != line {
+			return nil, fmt.Errorf("all records in the rrset must have the same line %s", line)
+		}
+		if r.Metadata[metaWeight] != weightStr {
+			return nil, fmt.Errorf("all records in the rrset must have the same weight %s", weightStr)
+		}
+	}
+
+	// parse weight to int32
+	var weight *int32
+	if weightStr != "" {
+		weightInt, err := strconv.ParseInt(weightStr, 10, 32)
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse weight %s to int32", weightStr)
+		}
+		weightInt32 := int32(weightInt)
+		// weight should be 0-1000
+		if weightInt32 < 0 || weightInt32 > 1000 {
+			return nil, fmt.Errorf("weight must be between 0 and 1000")
+		}
+		weight = &weightInt32
+	}
+
 	resultTTL := int32(0)
 	resultVal := []string{}
 	name := expectedKey.NameFQDN + "."
-	result := &model.ListRecordSets{
+	key := rcs[0].Metadata[metaKey]
+	result := &model.ShowRecordSetByZoneResp{
 		Name:    &name,
 		Type:    &expectedKey.Type,
 		Ttl:     &resultTTL,
 		Records: &resultVal,
+		Line:    &line,
+		Weight:  weight,
+		Description: &key,
 	}
 
 	for _, r := range rcs {
@@ -84,5 +130,5 @@ func recordsToNative(rcs models.Records, expectedKey models.RecordKey) *model.Li
 		}
 	}
 
-	return result
+	return result, nil
 }
