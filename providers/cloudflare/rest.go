b6cd7a3fbb7082b2d310e94d66fa894521d30d1f
Josh Zhang <jzhang1@stackoverflow.com>
2024-07-01 15:24:41 -0400

    Catch zero item list error

--- providers/cloudflare/rest.go
+++ providers/cloudflare/rest.go
@@ -2,6 +2,7 @@ package cloudflare
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"strings"
 
@@ -278,7 +279,11 @@ func (c *cloudflareProvider) getUniversalSSL(domainID string) (bool, error) {
 func (c *cloudflareProvider) getSingleRedirects(id string, domain string) ([]*models.RecordConfig, error) {
 	rules, err := c.cfClient.GetEntrypointRuleset(context.Background(), cloudflare.ZoneIdentifier(id), "http_request_dynamic_redirect")
 	if err != nil {
-		return nil, fmt.Errorf("failed fetching redirect rule list cloudflare: %s", err)
+		var e *cloudflare.NotFoundError
+		if errors.As(err, &e) {
+			return []*models.RecordConfig{}, nil
+		}
+		return nil, fmt.Errorf("failed fetching redirect rule list cloudflare: [ %T ] %s", err, err)
 	}
 	//var rulelist []cloudflare.RulesetRule
 	//rulelist = rules.Rules
@@ -352,7 +357,8 @@ func (c *cloudflareProvider) createSingleRedirect(domainID string, cfr models.Cl
 
 	// Get a list of current redirects so that the new redirect get appended to it
 	rules, err := c.cfClient.GetEntrypointRuleset(context.Background(), cloudflare.ZoneIdentifier(domainID), "http_request_dynamic_redirect")
-	if err != nil {
+	var e *cloudflare.NotFoundError
+	if err != nil && !errors.As(err, &e) {
 		return fmt.Errorf("failed fetching redirect rule list cloudflare: %s", err)
 	}
 	newSingleRedirect.Rules = newSingleRedirectRules
