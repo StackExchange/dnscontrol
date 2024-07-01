05ab1641a593a600ebcbd037b9294065713d6f26
Josh Zhang <jzhang1@stackoverflow.com>
2024-07-01 17:19:51 -0400

    Remove trailing / that breaks URI

--- providers/cloudflare/singleredirect.go
+++ providers/cloudflare/singleredirect.go
@@ -132,12 +132,12 @@ func makeRuleFromPattern(pattern, replacement string, temporary bool) (string, s
 
 	} else if strings.Count(replacement, `$`) == 1 && rpath == `/$1` {
 		// https://i.sstatic.net/$1 ($1 at end)
-		expr = fmt.Sprintf(`concat("https://%s/", http.request.uri.path)`, rhost)
+		expr = fmt.Sprintf(`concat("https://%s", http.request.uri.path)`, rhost)
 
 	} else if strings.Count(host, `*`) == 1 && strings.Count(path, `*`) == 1 &&
 		strings.Count(replacement, `$`) == 1 && rpath == `/$2` {
 		// https://careers.stackoverflow.com/$2
-		expr = fmt.Sprintf(`concat("https://%s/", http.request.uri.path)`, rhost)
+		expr = fmt.Sprintf(`concat("https://%s", http.request.uri.path)`, rhost)
 
 	}
 
