f10963de8e278d822da5e280747f60c21b567df5
Tom Limoncelli <tlimoncelli@stackoverflow.com>
2024-07-02 16:28:39 -0400

    CLOUDFLARE: Fix another redirect bug

--- providers/cloudflare/singleredirect.go
+++ providers/cloudflare/singleredirect.go
@@ -149,7 +149,7 @@ func makeRuleFromPattern(pattern, replacement string, temporary bool) (string, s
 		expr = fmt.Sprintf(`concat("https://%s", http.request.uri.path)`, rhost)
 
 	} else if strings.Count(host, `*`) == 1 && strings.Count(path, `*`) == 1 &&
-		strings.Count(replacement, `$`) == 1 && strings.HasPrefix(rpath, `/$2`) {
+		strings.Count(replacement, `$`) == 1 && strings.HasSuffix(rpath, `/$2`) {
 		// https://careers.stackoverflow.com/$2
 		expr = fmt.Sprintf(`concat("https://%s", http.request.uri.path)`, rhost)
 
