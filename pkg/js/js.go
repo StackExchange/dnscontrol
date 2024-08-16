446cf7d6467624c1cc6c3461858019d8f50c97dd
fuero <108815+fuero@users.noreply.github.com>
2024-08-16 21:36:08 +0200

    gofmt

--- pkg/js/js.go
+++ pkg/js/js.go
@@ -5,8 +5,8 @@ import (
 	"crypto/sha256"
 	"crypto/sha512"
 	_ "embed" // Used to embed helpers.js in the binary.
-	"encoding/json"
 	"encoding/hex"
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
@@ -138,26 +138,26 @@ func hashFunc(call otto.FunctionCall) otto.Value {
 		throw(call.Otto, "require takes exactly two arguments")
 	}
 	algorithm := call.Argument(0).String() // The algorithm to use for hashing
-	value := call.Argument(1).String() // The value to hash
+	value := call.Argument(1).String()     // The value to hash
 	result := otto.Value{}
 	fmt.Printf("%s\n", value)
 
 	switch algorithm {
-		case "SHA1", "sha1":
-			tmp := sha1.New()
-			tmp.Write([]byte(value))
-			fmt.Printf("%s\n", hex.EncodeToString(tmp.Sum(nil)))
-			result, _ = otto.ToValue(hex.EncodeToString(tmp.Sum(nil)))
-		case "SHA256", "sha256":
-			tmp := sha256.New()
-			tmp.Write([]byte(value))
-			result, _ = otto.ToValue(hex.EncodeToString(tmp.Sum(nil)))
-		case "SHA512", "sha512":
-			tmp := sha512.New()
-			tmp.Write([]byte(value))
-			result, _ = otto.ToValue(hex.EncodeToString(tmp.Sum(nil)))
-		default:
-			throw(call.Otto, fmt.Sprintf("invalid algorithm %s given", algorithm))
+	case "SHA1", "sha1":
+		tmp := sha1.New()
+		tmp.Write([]byte(value))
+		fmt.Printf("%s\n", hex.EncodeToString(tmp.Sum(nil)))
+		result, _ = otto.ToValue(hex.EncodeToString(tmp.Sum(nil)))
+	case "SHA256", "sha256":
+		tmp := sha256.New()
+		tmp.Write([]byte(value))
+		result, _ = otto.ToValue(hex.EncodeToString(tmp.Sum(nil)))
+	case "SHA512", "sha512":
+		tmp := sha512.New()
+		tmp.Write([]byte(value))
+		result, _ = otto.ToValue(hex.EncodeToString(tmp.Sum(nil)))
+	default:
+		throw(call.Otto, fmt.Sprintf("invalid algorithm %s given", algorithm))
 	}
 	return result
 }
