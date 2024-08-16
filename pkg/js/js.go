d6b251b486c898945a8d4628f149b17bb50da552
fuero <108815+fuero@users.noreply.github.com>
2024-08-16 21:34:47 +0200

    Exposes sha1, sha256, and sha512 hashing functions to Javascript

--- pkg/js/js.go
+++ pkg/js/js.go
@@ -1,8 +1,12 @@
 package js
 
 import (
+	"crypto/sha1"
+	"crypto/sha256"
+	"crypto/sha512"
 	_ "embed" // Used to embed helpers.js in the binary.
 	"encoding/json"
+	"encoding/hex"
 	"fmt"
 	"log"
 	"os"
@@ -74,6 +78,7 @@ func ExecuteJavascriptString(script []byte, devMode bool, variables map[string]s
 	vm.Set("REVCOMPAT", reverseCompat)
 	vm.Set("glob", listFiles) // used for require_glob()
 	vm.Set("PANIC", jsPanic)
+	vm.Set("HASH", hashFunc)
 
 	// add cli variables to otto
 	for key, value := range variables {
@@ -125,6 +130,36 @@ func GetHelpers(devMode bool) string {
 
 	// Return the embedded bytes:
 	return helpersJsStatic
+}
+
+// Exposes sha1, sha256, and sha512 hashing functions to Javascript
+func hashFunc(call otto.FunctionCall) otto.Value {
+	if len(call.ArgumentList) != 2 {
+		throw(call.Otto, "require takes exactly two arguments")
+	}
+	algorithm := call.Argument(0).String() // The algorithm to use for hashing
+	value := call.Argument(1).String() // The value to hash
+	result := otto.Value{}
+	fmt.Printf("%s\n", value)
+
+	switch algorithm {
+		case "SHA1", "sha1":
+			tmp := sha1.New()
+			tmp.Write([]byte(value))
+			fmt.Printf("%s\n", hex.EncodeToString(tmp.Sum(nil)))
+			result, _ = otto.ToValue(hex.EncodeToString(tmp.Sum(nil)))
+		case "SHA256", "sha256":
+			tmp := sha256.New()
+			tmp.Write([]byte(value))
+			result, _ = otto.ToValue(hex.EncodeToString(tmp.Sum(nil)))
+		case "SHA512", "sha512":
+			tmp := sha512.New()
+			tmp.Write([]byte(value))
+			result, _ = otto.ToValue(hex.EncodeToString(tmp.Sum(nil)))
+		default:
+			throw(call.Otto, fmt.Sprintf("invalid algorithm %s given", algorithm))
+	}
+	return result
 }
 
 func require(call otto.FunctionCall) otto.Value {
