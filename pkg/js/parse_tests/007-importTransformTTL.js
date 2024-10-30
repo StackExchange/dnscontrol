928d6395a96843af23147b5babfc265ef94b1559
Tom Limoncelli <tlimoncelli@stackoverflow.com>
2024-10-30 14:32:28 -0400

    wip!

--- pkg/js/parse_tests/007-importTransformTTL.js
+++ pkg/js/parse_tests/007-importTransformTTL.js
@@ -1,5 +1,20 @@
-var TRANSFORM_INT = [ {low: "0.0.0.0", high: "1.1.1.1", newIP: "2.2.2.2" } ]
-var TRANSFORM_BASE = [ {low: "0.0.0.0", high: "1.1.1.1", newBase: "4.4.4.4" } ]
-D("foo1.com","reg");
-D("foo2.com","reg",IMPORT_TRANSFORM(TRANSFORM_BASE,"int2.com",60))
-D("foo3.com","reg",IMPORT_TRANSFORM_STRIP(TRANSFORM_INT,"int3.com",99,".com"))
+var TRANSFORM_NEWIP = [{
+    low: "0.0.0.0",
+    high: "1.1.1.1",
+    newIP: "2.2.2.2"
+}];
+var TRANSFORM_BASE = [{
+    low: "0.0.0.0",
+    high: "1.1.1.1",
+    newBase: "4.4.4.4"
+}];
+
+D("foo1.com", "reg");
+
+D("foo2.com", "reg",
+    IMPORT_TRANSFORM(TRANSFORM_BASE, "int2.com", 60)
+);
+
+D("foo3.com", "reg",
+    IMPORT_TRANSFORM_STRIP(TRANSFORM_NEWIP, "int3.com", 99, ".com")
+);
