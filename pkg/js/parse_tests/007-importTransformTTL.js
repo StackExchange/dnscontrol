39d87c990ebbe85e3e5468f22dceb88b97bb81c1
Tom Limoncelli <tlimoncelli@stackoverflow.com>
2024-10-31 10:25:40 -0400

    add format_tt test, fix parse_test/007

--- pkg/js/parse_tests/007-importTransformTTL.js
+++ pkg/js/parse_tests/007-importTransformTTL.js
@@ -7,14 +7,19 @@ var TRANSFORM_BASE = [{
     low: "0.0.0.0",
     high: "1.1.1.1",
     newBase: "4.4.4.4"
-}];
+}, {
+    low: "7.7.7.7",
+    high: "8.8.8.8",
+    newBase: "9.9.9.9"
+},
+];
 
 D("foo1.com", "reg");
 
 D("foo2.com", "reg",
-    IMPORT_TRANSFORM(TRANSFORM_BASE, "int2.com", 60)
+    IMPORT_TRANSFORM(TRANSFORM_BASE, "foo1.com", 60)
 );
 
 D("foo3.com", "reg",
-    IMPORT_TRANSFORM_STRIP(TRANSFORM_NEWIP, "int3.com", 99, ".com")
+    IMPORT_TRANSFORM_STRIP(TRANSFORM_NEWIP, "foo1.com", 99, ".com")
 );
