5267123d648bcbd597c2d3e41aac5db0da2069ea
Tom Limoncelli <tlimoncelli@stackoverflow.com>
2024-11-14 11:40:03 -0500

    NAMECHEAP: Enable cmode concurrent

--- providers/namecheap/namecheapProvider.go
+++ providers/namecheap/namecheapProvider.go
@@ -29,7 +29,7 @@ var features = providers.DocumentationNotes{
 	// The default for unlisted capabilities is 'Cannot'.
 	// See providers/capabilities.go for the entire list of capabilities.
 	providers.CanGetZones:            providers.Can(),
-	providers.CanConcur:              providers.Cannot(),
+	providers.CanConcur:              providers.Can(),
 	providers.CanUseAlias:            providers.Can(),
 	providers.CanUseCAA:              providers.Can(),
 	providers.CanUseLOC:              providers.Cannot(),
