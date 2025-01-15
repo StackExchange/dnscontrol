7deba90a6bad53d01b2635997a93f8d528a9e50b
Tom Limoncelli <tlimoncelli@stackoverflow.com>
2025-01-15 18:11:34 -0500

    move retry logic to retry.go

--- providers/transip/retry.go
+++ providers/transip/retry.go
@@ -47,3 +47,30 @@ func retryNeeded(err error) bool {
 
 	return true // Request the API call be re-tried.
 }
+
+/*
+
+TODO(tlim): Use X-Rate-Limit-Reset to optimize sleep time.
+
+This is a rather lazy implementation.  A better implementation would examine
+the X-Rate-Limit-Reset header and wait until that timestamp, if the timestamp
+seems reasonable.  This implementation just does an exponential back-off.
+
+This is what the documentation says:
+
+> **Rate limit**
+> The rate limit for this API uses a sliding window of 15 minutes. Within this window, a maximum of 1000 requests can be made per user.
+>
+> Every request returns the following headers, indicating the number of requests made within this window, the amount of requests remaining and the reset timestamp.
+>
+> ```text
+> X-Rate-Limit-Limit: 1000
+> X-Rate-Limit-Remaining: 650
+> X-Rate-Limit-Reset: 1485875578
+> ```
+
+> When this rate limit is exceeded, the response contains an error with HTTP status code: `429: Too many requests`.
+
+<https://api.transip.nl/rest/docs.html#header-rate-limit>
+
+*/
