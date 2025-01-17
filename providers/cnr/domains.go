6e29f942fe2ae88275cbe47187b396b0784d5f16
Kai Schwarz <kai.schwarz@centralnic.com>
2025-01-17 15:23:44 +0100

    CNR: reviewed EnsureZoneExists

--- providers/cnr/domains.go
+++ providers/cnr/domains.go
@@ -4,31 +4,20 @@ package cnr
 // * if access to dnszone is not allowed (not authorized) or
 // * if it doesn't exist and creating it fails
 func (n *CNRClient) EnsureZoneExists(domain string) error {
-	r := n.client.Request(map[string]interface{}{
-		"COMMAND": "StatusDNSZone",
+	command := map[string]interface{}{
+		"COMMAND": "AddDNSZone",
 		"DNSZONE": domain,
-	})
-	code := r.GetCode()
-	if code == 545 {
-		command := map[string]interface{}{
-			"COMMAND": "AddDNSZone",
-			"DNSZONE": domain,
-		}
-		if n.APIEntity == "OTE" {
-			command["SOATTL"] = "33200"
-			command["SOASERIAL"] = "0000000000"
-		}
-		// Create the zone
-		r = n.client.Request(command)
-		if !r.IsSuccess() {
-			return n.GetCNRApiError("Failed to create not existing zone ", domain, r)
-		}
-	} else if code == 531 {
-		return n.GetCNRApiError("Not authorized to manage dnszone", domain, r)
-	} else if r.IsError() || r.IsTmpError() {
-		return n.GetCNRApiError("Error while checking status of dnszone", domain, r)
 	}
-	return nil
+	if n.APIEntity == "OTE" {
+		command["SOATTL"] = "33200"
+		command["SOASERIAL"] = "0000000000"
+	}
+	// Create the zone
+	r := n.client.Request(command)
+	if r.GetCode() == 549 || r.IsSuccess() {
+		return nil
+	}
+	return n.GetCNRApiError("Failed to create not existing zone ", domain, r)
 }
 
 // ListZones lists all the
