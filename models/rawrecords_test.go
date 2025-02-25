b65191af424a163602129f680e069d99d6e6e8e1
Tom Limoncelli <tlimoncelli@stackoverflow.com>
2025-02-25 12:08:01 -0500

    m

---
+++ models/rawrecords_test.go
@@ -0,0 +1,22 @@
+package models
+
+// func TestEffectiveLabel(t *testing.T) {
+// 	tests := []struct {
+// 		short    string
+// 		sub      string
+// 		expected string
+// 	}{
+// 		{"", "sub", "sub"},
+// 		{"@", "sub", "sub"},
+// 		{"short", "sub", "short.sub"},
+// 	}
+
+// 	for _, test := range tests {
+// 		t.Run(test.short+"_"+test.sub, func(t *testing.T) {
+// 			result := effectiveLabel(test.short, test.sub)
+// 			if result != test.expected {
+// 				t.Errorf("expected %q, got %q", test.expected, result)
+// 			}
+// 		})
+// 	}
+// }
---
+++ pkg/js/parse_tests/052-a.js
@@ -0,0 +1,17 @@
+var REG = NewRegistrar("Third-Party", "NONE");
+var CF = NewDnsProvider("Cloudflare", "CLOUDFLAREAPI");
+
+D("foo.com", REG, DnsProvider(CF),
+    A("@", "1.1.1.1"),
+    A("www", "2.2.2.2"),
+);
+
+D_EXTEND("foo.com",
+    A("@", "3.3.3.3"),
+    A("ewww", "4.4.4.4"),
+);
+
+D_EXTEND("sub.foo.com",
+    A("@", "5.5.5.5"),
+    A("swww", "6.6.6.6"),
+);
