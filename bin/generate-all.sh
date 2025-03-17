0d8dcea63a7e668886d0e0ea8ee02cdf78c15347
Tom Limoncelli <tlimoncelli@stackoverflow.com>
2025-03-17 09:06:32 -0400

    BUILD: Move regen steps to a script (#3491)

---
+++ bin/generate-all.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+go fmt ./...
+
+if [[ -x node_modules/.bin/prettier ]]; then
+  node_modules/.bin/prettier --write pkg/js/helpers.js
+fi
+
+# JSON
+bin/fmtjson $(find . -type f -name \*.json -print)
+
+# dnsconfig.js-compatible files:
+for i in pkg/js/parse_tests/*.js ; do dnscontrol fmt -i $i -o $i ; done
+
+go generate ./...
+
+go mod tidy
