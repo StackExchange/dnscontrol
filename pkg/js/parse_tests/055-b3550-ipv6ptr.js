8381432eea90f92a13c7362e3bc5101c388ff774
Tom Limoncelli <tlimoncelli@stackoverflow.com>
2025-04-28 16:21:57 -0400

    parse_test/055 test valid, code failing

--- pkg/js/parse_tests/055-b3550-ipv6ptr.js
+++ pkg/js/parse_tests/055-b3550-ipv6ptr.js
@@ -2,15 +2,15 @@ var REG_NONE = NewRegistrar("none");
 var DSP_BIND = NewDnsProvider("bind", "BIND");
 
 D(REV("2011:abcd::/32"), REG_NONE, DnsProvider(DSP_BIND),
-    PTR("2011:abcd::11", "another11.example.com."),
-    PTR(REV("2011:abcd::11"), "another22.example.com."),
+    PTR("2011:abcd::11", "host11.example.com."),
+    PTR(REV("2011:abcd::22"), "host22.example.com."),
 );
 
 D(REV("2001:db8::/32"), REG_NONE, DnsProvider(DSP_BIND),
-    PTR("2001:db8::11", "reverse11.example.com."),
-    PTR(REV("2001:db8::22"), "reverse22.example.com."),
+    PTR("2001:db8::11", "server11.example.com."),
+    PTR(REV("2001:db8::22"), "server22.example.com."),
 );
 
 D_EXTEND("d.c.b.a.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.8.b.d.0.1.0.0.2.ip6.arpa",
-    PTR("2001:db8::abcd", "extend.example.com.")
+    PTR("2001:db8::abcd", "abcd.example.com.")
 );
