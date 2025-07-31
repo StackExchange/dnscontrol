48946adf491ff8c9061d50d2007e8b4a27dcb005
Alex Trull <git-and-sig.alex@trull.org>
2025-07-31 11:01:49 +0200

    refactored auditrecords closer to adguard style

--- pkg/rejectif/ns.go
+++ pkg/rejectif/ns.go
@@ -11,7 +11,7 @@ import (
 // NsAtApex detects NS records at the apex/root domain.
 // Use this when a provider doesn't support custom NS records at the apex.
 func NsAtApex(rc *models.RecordConfig) error {
-	if rc.GetLabel() == "@" {
+	if rc.GetLabel() == "" {
 		return errors.New("NS records not supported at apex")
 	}
 	return nil
--- providers/joker/auditrecords.go
+++ providers/joker/auditrecords.go
@@ -27,24 +27,24 @@ func AuditRecords(records []*models.RecordConfig) []error {
 
 	// Joker does not support custom NS records at apex (domain root)
 	// Joker automatically manages apex NS records
-	a.Add("NS", rejectif.NsAtApex) // Last verified 2025-01-31
+	a.Add("NS", rejectif.NsAtApex)
 
 	// Joker has round-trip issues with TXT records containing unbalanced quotes
-	a.Add("TXT", rejectif.TxtHasUnpairedDoubleQuotes) // Last verified 2025-01-31
+	a.Add("TXT", rejectif.TxtHasUnpairedDoubleQuotes)
 
 	// Joker has round-trip issues with TXT records containing backslashes
-	a.Add("TXT", rejectif.TxtHasBackslash) // Last verified 2025-01-31
+	a.Add("TXT", rejectif.TxtHasBackslash)
 
 	// SRV records must have valid port and target
-	a.Add("SRV", rejectif.SrvHasZeroPort) // Last verified 2025-01-31
-	a.Add("SRV", rejectif.SrvHasEmptyTarget) // Last verified 2025-01-31
+	a.Add("SRV", rejectif.SrvHasZeroPort)
+	a.Add("SRV", rejectif.SrvHasEmptyTarget)
 
 	// CAA records must have valid tag and target
-	a.Add("CAA", rejectif.CaaHasEmptyTag) // Last verified 2025-01-31
-	a.Add("CAA", rejectif.CaaHasEmptyTarget) // Last verified 2025-01-31
+	a.Add("CAA", rejectif.CaaHasEmptyTag)
+	a.Add("CAA", rejectif.CaaHasEmptyTarget)
 
 	// NAPTR records must have a replacement
-	a.Add("NAPTR", rejectif.NaptrHasEmptyTarget) // Last verified 2025-01-31
+	a.Add("NAPTR", rejectif.NaptrHasEmptyTarget)
 
 	errors := []error{}
 	errors = append(errors, a.Audit(records)...)
