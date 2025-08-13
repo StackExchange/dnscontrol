b3d2529c5bd058c0d75ad80afdbde56a282f3dec
pieterjan.eilers <pieterjan.eilers@realtimeregister.com>
2025-08-13 15:15:38 +0200

    DNSC-3610: include SRV

--- providers/realtimeregister/realtimeregisterProvider.go
+++ providers/realtimeregister/realtimeregisterProvider.go
@@ -267,7 +267,7 @@ func toRecord(recordConfig *models.RecordConfig) Record {
 		if record.Content == "" {
 			record.Content = "."
 		}
-		record.Priority = int(recordConfig.SrvPriority)
+		record.Priority = parsePriority(int(recordConfig.SrvPriority))
 		record.Content = fmt.Sprintf("%d %d %s", recordConfig.SrvWeight, recordConfig.SrvPort, record.Content)
 	case "NAPTR", "SSHFP", "TLSA", "CAA":
 		record.Content = recordConfig.GetTargetCombined()
@@ -279,13 +279,9 @@ func toRecord(recordConfig *models.RecordConfig) Record {
 	case "MX":
 		if record.Content == "" {
 			record.Content = "."
-			record.Priority = 0
-		} else {
-			record.Priority = int(recordConfig.MxPreference)
-		}
-		// Workaround for 0 prio and 'omitempty' restrictions on json marshalling
-		if record.Priority == 0 {
 			record.Priority = -1
+		} else {
+			record.Priority = parsePriority(int(recordConfig.MxPreference))
 		}
 	case "LOC":
 		parts := strings.Fields(recordConfig.GetTargetCombined())
@@ -304,6 +300,14 @@ func toRecord(recordConfig *models.RecordConfig) Record {
 	}
 
 	return *record
+}
+
+func parsePriority(priority int) int {
+	// Workaround for 0 prio and 'omitempty' restrictions on json marshalling
+	if priority == 0 {
+		return -1
+	}
+	return priority
 }
 
 func (api *realtimeregisterAPI) EnsureZoneExists(domain string) error {
