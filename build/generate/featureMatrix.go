28df4db8f19f09f3fdb8847324dcb8002d285fe3
Louis Laureys <louis@laureys.me>
2025-09-12 23:51:21 +0200

    DOCS: Add tooltips and distinction between not supported and not implemented to feature matrix

--- build/generate/featureMatrix.go
+++ build/generate/featureMatrix.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"html"
 	"os"
 	"sort"
 	"strings"
@@ -85,16 +86,28 @@ func featureEmoji(
 	featureMap FeatureMap,
 	featureName string,
 ) string {
+	var emoji string
+	var tooltip string
+
 	if featureMap[featureName] == nil {
-		return "❔"
+		emoji = "❔"
+		tooltip = "Unknown"
+	} else if featureMap[featureName].HasFeature {
+		emoji = "✅"
+		tooltip = "Supported"
+	} else if featureMap[featureName].Unimplemented {
+		emoji = "❓"
+		tooltip = "Not implemented"
+	} else {
+		emoji = "❌"
+		tooltip = "Not supported"
 	}
 
-	if featureMap[featureName].HasFeature {
-		return "✅"
-	} else if featureMap[featureName].Unimplemented {
-		return "❔"
+	if featureMap[featureName] != nil && featureMap[featureName].Comment != "" {
+		tooltip += ": " + featureMap[featureName].Comment
 	}
-	return "❌"
+
+	return fmt.Sprintf("<span title=\"%s\">%s</span>", html.EscapeString(tooltip), emoji)
 }
 
 func matrixData() *FeatureMatrix {
