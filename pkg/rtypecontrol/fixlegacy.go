5a2ab9da7bfbbae3510a103dd6e08555599f7f43
Thomas Limoncelli <tlimoncelli@stackoverflow.com>
2026-01-22 10:41:14 -0500

    fix legacy code updating .F

---
+++ pkg/rtypecontrol/fixlegacy.go
@@ -0,0 +1,26 @@
+package rtypecontrol
+
+import "github.com/StackExchange/dnscontrol/v4/models"
+
+// FixLegacyAll populates .F to compenstate for providers that have not been
+// updated to support RecordConfigV2 when creating RecordConfig.
+// It is called anywhere dc.PostProcess() is called.  dc.PostProcess() can't
+// call it directly because that would cause an import cycle.
+func FixLegacyAll(dc *models.DomainConfig) {
+	for _, rec := range dc.Records {
+
+		FixLegacyRecords(rec)
+	}
+}
+
+// FixLegacyRecords populates .F to compenstate for providers that have not been
+func FixLegacyRecords(rec *models.RecordConfig) {
+	// Populate .F if needed:
+	// That is... If rec.F == nil and this is a "moderne" type.
+	if rec.F != nil {
+		return
+	}
+	if fixer, ok := Func[rec.Type]; ok {
+		fixer.CopyFromLegacyFields(rec)
+	}
+}
