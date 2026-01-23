6bd9b2f8ad0954e57266a6f94c3ae25c0e8802a7
Thomas Limoncelli <tlimoncelli@stackoverflow.com>
2026-01-23 11:01:18 -0500

    REFACTOR: Move modesl.RRtoRC* to its own package dnsrr

--- pkg/diff2/handsoff_test.go
+++ pkg/diff2/handsoff_test.go
@@ -18,7 +18,7 @@ func parseZoneContents(content string, zoneName string, zonefileName string) (mo
 
 	foundRecords := models.Records{}
 	for rr, ok := zp.Next(); ok; rr, ok = zp.Next() {
-		rec, err := models.RRtoRCTxtBug(rr, zoneName)
+		rec, err := dnsrr.RRtoRCTxtBug(rr, zoneName)
 		if err != nil {
 			return nil, err
 		}
---
+++ pkg/dnsrr/dnsrr.go
@@ -0,0 +1,107 @@
+package dnsrr
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/StackExchange/dnscontrol/v4/models"
+	"github.com/StackExchange/dnscontrol/v4/pkg/domaintags"
+	"github.com/StackExchange/dnscontrol/v4/pkg/rtypecontrol"
+	"github.com/StackExchange/dnscontrol/v4/pkg/rtypeinfo"
+	"github.com/miekg/dns"
+)
+
+// RRtoRC converts dns.RR to models.RecordConfig
+func RRtoRC(rr dns.RR, origin string) (models.RecordConfig, error) {
+	return helperRRtoRC(rr, origin, false)
+}
+
+// RRtoRCTxtBug converts dns.RR to models.RecordConfig. Compensates for the backslash bug in github.com/miekg/dns/issues/1384.
+func RRtoRCTxtBug(rr dns.RR, origin string) (models.RecordConfig, error) {
+	return helperRRtoRC(rr, origin, true)
+}
+
+// helperRRtoRC converts dns.RR to models.RecordConfig. If fixBug is true, replaces `\\` to `\` in TXT records to compensate for github.com/miekg/dns/issues/1384.
+func helperRRtoRC(rr dns.RR, origin string, fixBug bool) (models.RecordConfig, error) {
+	// Convert's dns.RR into DNSControl's models.RecordConfig struct.
+
+	header := rr.Header()
+	ty := dns.TypeToString[header.Rrtype]
+
+	if rtypeinfo.IsModernType(ty) {
+		switch v := rr.(type) {
+		default:
+			rec, err := rtypecontrol.NewRecordConfigFromStruct(strings.TrimSuffix(header.Name, origin), header.Ttl, dns.TypeToString[header.Rrtype], v, domaintags.MakeDomainNameVarieties(origin))
+			return *rec, err
+		}
+	}
+
+	rc := new(models.RecordConfig)
+	rc.Type = dns.TypeToString[header.Rrtype]
+	rc.TTL = header.Ttl
+	rc.Original = rr
+	rc.SetLabelFromFQDN(strings.TrimSuffix(header.Name, "."), origin)
+	var err error
+	switch v := rr.(type) { // #rtype_variations
+	case *dns.A:
+		err = rc.SetTarget(v.A.String())
+	case *dns.AAAA:
+		err = rc.SetTarget(v.AAAA.String())
+	case *dns.CAA:
+		err = rc.SetTargetCAA(v.Flag, v.Tag, v.Value)
+	case *dns.CNAME:
+		err = rc.SetTarget(v.Target)
+	case *dns.DHCID:
+		err = rc.SetTarget(v.Digest)
+	case *dns.DNAME:
+		err = rc.SetTarget(v.Target)
+	case *dns.DS:
+		panic("DS should be handled as modern type")
+	case *dns.DNSKEY:
+		err = rc.SetTargetDNSKEY(v.Flags, v.Protocol, v.Algorithm, v.PublicKey)
+	case *dns.HTTPS:
+		err = rc.SetTargetSVCB(v.Priority, v.Target, v.Value)
+	case *dns.LOC:
+		err = rc.SetTargetLOC(v.Version, v.Latitude, v.Longitude, v.Altitude, v.Size, v.HorizPre, v.VertPre)
+	case *dns.MX:
+		err = rc.SetTargetMX(v.Preference, v.Mx)
+	case *dns.NAPTR:
+		err = rc.SetTargetNAPTR(v.Order, v.Preference, v.Flags, v.Service, v.Regexp, v.Replacement)
+	case *dns.OPENPGPKEY:
+		err = rc.SetTarget(v.PublicKey)
+	case *dns.NS:
+		err = rc.SetTarget(v.Ns)
+	case *dns.PTR:
+		err = rc.SetTarget(v.Ptr)
+	case *dns.RP:
+		panic("RP should be handled as modern type")
+	case *dns.SMIMEA:
+		err = rc.SetTargetSMIMEA(v.Usage, v.Selector, v.MatchingType, v.Certificate)
+	case *dns.SOA:
+		err = rc.SetTargetSOA(v.Ns, v.Mbox, v.Serial, v.Refresh, v.Retry, v.Expire, v.Minttl)
+	case *dns.SRV:
+		err = rc.SetTargetSRV(v.Priority, v.Weight, v.Port, v.Target)
+	case *dns.SSHFP:
+		err = rc.SetTargetSSHFP(v.Algorithm, v.Type, v.FingerPrint)
+	case *dns.SVCB:
+		err = rc.SetTargetSVCB(v.Priority, v.Target, v.Value)
+	case *dns.TLSA:
+		err = rc.SetTargetTLSA(v.Usage, v.Selector, v.MatchingType, v.Certificate)
+	case *dns.TXT:
+		if fixBug {
+			t := strings.Join(v.Txt, "")
+			te := t
+			te = strings.ReplaceAll(te, `\\`, `\`)
+			te = strings.ReplaceAll(te, `\"`, `"`)
+			err = rc.SetTargetTXT(te)
+		} else {
+			err = rc.SetTargetTXTs(v.Txt)
+		}
+	default:
+		return *rc, fmt.Errorf("rrToRecord: Unimplemented zone record type=%s (%v)", rc.Type, rr)
+	}
+	if err != nil {
+		return *rc, fmt.Errorf("unparsable record received: %w", err)
+	}
+	return *rc, nil
+}
