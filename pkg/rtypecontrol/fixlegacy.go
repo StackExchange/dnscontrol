05286eae868110dd276e36a2194de5db4f59a78f
Thomas Limoncelli <tlimoncelli@stackoverflow.com>
2026-01-23 10:01:30 -0500

    FixLegacy

--- pkg/rtypecontrol/fixlegacy.go
+++ pkg/rtypecontrol/fixlegacy.go
@@ -2,21 +2,31 @@ package rtypecontrol
 
 import "github.com/StackExchange/dnscontrol/v4/models"
 
-// FixLegacyAll populates .F to compenstate for providers that have not been
+// FixLegacyDC populates .F to compenstate for providers that have not been
 // updated to support RecordConfigV2 when creating RecordConfig.
-// It is called anywhere dc.PostProcess() is called.  dc.PostProcess() can't
-// call it directly because that would cause an import cycle.
-func FixLegacyAll(dc *models.DomainConfig) {
-	for _, rec := range dc.Records {
+// It is called anywhere dc.PostProcess() or models.PostProcessRecords() is
+// called.  Those functions can't call it directly because that would cause an
+// import cycle.
+func FixLegacyDC(dc *models.DomainConfig) {
+	FixLegacyRecords(&dc.Records)
+}
 
+// FixLegacyRecords populates .F to compenstate for providers that have not been
+// updated to support RecordConfigV2 when creating RecordConfig.
+// It is called anywhere provider.GetZoneRecords() is called. GetZoneRecords()
+// can't call it directly because that would involve modifying every provider.
+// Instead, providers should be fixed to generate records properly.
+func FixLegacyRecords(recs *models.Records) {
+	for _, rec := range *recs {
 		FixLegacyRecord(rec)
 	}
 }
 
 // FixLegacyRecord populates .F to compenstate for providers that have not been
+// updated to support RecordConfigV2 when creating RecordConfig.
 func FixLegacyRecord(rec *models.RecordConfig) {
 	// Populate .F if needed:
-	// That is... If rec.F == nil and this is a "moderne" type.
+	// That is... If rec.F == nil and this is a "modern" type.
 	if rec.F != nil {
 		return
 	}
