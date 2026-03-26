b6895d86558a77e8cbc8d26e74bc75d0076ccb3c
Paul Sütterlin <psuet@netz.ac>
2026-03-26 18:23:12 +0100

    RWTH: Migrate to diff2

--- providers/rwth/dns.go
+++ providers/rwth/dns.go
@@ -2,7 +2,7 @@ package rwth
 
 import (
 	"github.com/StackExchange/dnscontrol/v4/models"
-	"github.com/StackExchange/dnscontrol/v4/pkg/diff"
+	"github.com/StackExchange/dnscontrol/v4/pkg/diff2"
 )
 
 // RWTHDefaultNs is the default DNS NS for this provider.
@@ -32,38 +32,42 @@ func (api *rwthProvider) GetNameservers(domain string) ([]*models.Nameserver, er
 func (api *rwthProvider) GetZoneRecordsCorrections(dc *models.DomainConfig, existingRecords models.Records) ([]*models.Correction, int, error) {
 	domain := dc.Name
 
-	toReport, create, del, modify, actualChangeCount, err := diff.NewCompat(dc).IncrementalDiff(existingRecords)
+	instructions, actualChangeCount, err := diff2.ByRecord(existingRecords, dc, nil)
 	if err != nil {
 		return nil, 0, err
 	}
-	// Start corrections with the reports
-	corrections := diff.GenerateMessageCorrections(toReport)
+	var corrections []*models.Correction
 
-	for _, d := range create {
-		des := d.Desired
-		corrections = append(corrections, &models.Correction{
-			Msg: d.String(),
-			F:   func() error { return api.createRecord(des) },
-		})
-	}
-	for _, d := range del {
-		existingRecord := d.Existing.Original.(RecordReply)
-		corrections = append(corrections, &models.Correction{
-			Msg: d.String(),
-			F:   func() error { return api.destroyRecord(existingRecord) },
-		})
-	}
-	for _, d := range modify {
-		rec := d.Desired
-		existingID := d.Existing.Original.(RecordReply).ID
-		corrections = append(corrections, &models.Correction{
-			Msg: d.String(),
-			F:   func() error { return api.updateRecord(existingID, *rec) },
-		})
+	for _, inst := range instructions {
+		switch inst.Type {
+		case diff2.REPORT:
+			corrections = append(corrections, &models.Correction{Msg: inst.MsgsJoined})
+		case diff2.CREATE:
+			rec := inst.New[0]
+			corrections = append(corrections, &models.Correction{
+				Msg: inst.MsgsJoined,
+				F:   func() error { return api.createRecord(rec) },
+			})
+		case diff2.DELETE:
+			existingRecord := inst.Old[0].Original.(RecordReply)
+			corrections = append(corrections, &models.Correction{
+				Msg: inst.MsgsJoined,
+				F:   func() error { return api.destroyRecord(existingRecord) },
+			})
+		case diff2.CHANGE:
+			rec := inst.New[0]
+			existingID := inst.Old[0].Original.(RecordReply).ID
+			corrections = append(corrections, &models.Correction{
+				Msg: inst.MsgsJoined,
+				F:   func() error { return api.updateRecord(existingID, *rec) },
+			})
+		default:
+			panic("unhandled instruction type")
+		}
 	}
 
 	// And deploy if any corrections were applied
-	if len(corrections) > 0 {
+	if actualChangeCount > 0 {
 		corrections = append(corrections, &models.Correction{
 			Msg: "Deploy zone " + domain,
 			F:   func() error { return api.deployZone(domain) },
