703e1bf97d526aa8c83b55176769121309da1042
Tom Limoncelli <tal@whatexit.org>
2026-05-29 07:48:07 -0400

    m

--- pkg/privatetypes/CLONE.sh
+++ pkg/privatetypes/CLONE.sh
@@ -1,13 +1,17 @@
 #!bin/bash
 
+SRC="Adguardhome_AAAA_Passthrough"
+SRC_UC=$( echo "${SRC}" | tr a-z A-Z )
+SRC_LC=$( echo "${SRC}" | tr A-Z a-z )
+
 DEST=$1 ; shift
 
 DEST_UC=$( echo "${DEST}" | tr a-z A-Z )
 DEST_LC=$( echo "${DEST}" | tr A-Z a-z )
 
-sed -e 's@ALIAS@'${DEST_UC}'@g'  				< alias.go       > ${DEST_LC}.go
-sed -e 's@ALIAS@'${DEST_UC}'@g' -e 's@Alias@'${DEST}'@g'	< alias_test.go  > ${DEST_LC}_test.go
-sed -e 's@ALIAS@'${DEST_UC}'@g'   				< rdata/alias.go > rdata/${DEST_LC}.go
+sed -e 's@'${SRC_UC}'@'${DEST_UC}'@g'  				   < "${SRC_LC}.go"       > "${DEST_LC}.go"
+sed -e 's@'${SRC_UC}'@'${DEST_UC}'@g' -e 's@'${SRC}'@'${DEST}'@g'  < "${SRC_LC}_test.go"  > "${DEST_LC}_test.go"
+sed -e 's@'${SRC_UC}'@'${DEST_UC}'@g'   			   < "rdata/${SRC_LC}.go" > "rdata/${DEST_LC}.go"
 
 echo 'Remember to fix:'
 echo 'models/fixhack.go'
